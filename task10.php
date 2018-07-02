713df2debffa858497e349e5f12583faab0c38b1
Piotr <pittero1@wp.pl>
2018-07-02 16:40:03 +0200

    kolorowa tablica zadanie 10-poprawia widoku v2

--- task10.php
+++ task10.php
@@ -2,6 +2,7 @@
 $tablica1 = range(0,19);
 $tablica2 = range("A", "T");
 echo '<table border="1" width="75%" height="100%">';
+$i=0;
 foreach($tablica1 as $key1 => $value1)
 {
 	echo '<tr>';
@@ -9,6 +10,7 @@ foreach($tablica1 as $key1 => $value1)
 	{
 		if(($key1%2 == 0 && $key2%2 == 0) || ($key1%2 == 1 && $key2%2 == 1))
 		{
+			$i++;
 			echo '<td bgcolor="#7f0000">';
 			echo $value1.$value2.' ';
 			echo '</td>';
