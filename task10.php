77d75a62de779e8f6eeb1f0ff7583ea4c2834f91
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-02 16:28:23 +0200

    Pokolorowanie siatki w task10.php

--- task10.php
+++ task10.php
@@ -1,12 +1,21 @@
 <?php
 	$tablica = array();
+	$color1 = 'style="background-color: yellow"';
+	$color2 = 'style="background-color: brown"';
+	
 	
 	echo "<table border=1>";
 	foreach (range(1, 20) as $col) {
 		echo "<tr>";
 		foreach (range('A', 'T') as $row => $val) {
 			$tablica[$col][$row] = $col . $val;
-			echo "<td>" . $tablica[$col][$row] . "</td>";
+			if (($col + $row) % 2 == 0) {
+				$color = $color1;
+			}
+			else {
+				$color = $color2;
+			}
+			echo "<td " . $color . ">" . $tablica[$col][$row] . "</td>";
 		}
 		echo "</tr>";
 	}
