4d1bd57662cd573650df3d8788514f7c8fc854c9
Kacper Lisicki <lisicki.biznes@gmail.com>
2018-07-02 14:24:20 +0200

    task1 vol3

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-// \/ \/ \/ TASK 1 \/ \/ \/
 echo 'Uczestniczę w "PHP CAMP" #2<br>';
 echo 'Na $BANK się czegoś nauczę!<br>';
 echo 'Jak to usunąłeś C:\\\moje_filmy?<br>';
@@ -13,11 +12,3 @@ echo "Jak to usunąłeś C:\\\moje_filmy?<br>";
 echo "Szklanka jest w 1\\n pełna<br>";
 echo "Dla n = 2 szklanka jest w \u{00bd} pełna<br>";
 echo "I'm a little mama's boy<br>";
-// /\ /\ /\ TASK 1 /\ /\ /\
-
-// \/ \/ \/ TASK 2 \/ \/ \/
-// /\ /\ /\ TASK 2 /\ /\ /\
-
-// \/ \/ \/ TASK 3 \/ \/ \/
-
-// /\ /\ /\ TASK 3 /\ /\ /\
