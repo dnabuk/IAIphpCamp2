309a7321f849eda12253f51fcda909a64a9716fb
Kacper Lisicki <lisicki.biznes@gmail.com>
2018-07-02 16:31:56 +0200

    task4 dziwne

--- taks4.php
+++ taks4.php
@@ -5,7 +5,12 @@ $t2 = range('A', 'T');
 foreach ($t1 as $a){
     echo "<div>";
     foreach ($t2 as $b) {
-        echo '<div style="display: inline-block; background-color:white; width: 50px; height: 50px; border: solid 1px black;">';
+        for($i = 1; $i <=20; $i++) {
+            if ($i % 2)
+                echo '<div style="display: inline-block; background-color:white; width: 50px; height: 50px; border: solid 1px black;">';
+            else
+                echo '<div style="display: inline-block; background-color:red; width: 50px; height: 50px; border: solid 1px black;">';
+        }
         echo $a.$b;
         echo '</div>';
     };
