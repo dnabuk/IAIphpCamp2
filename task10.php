f33eba24d6798b0f4ee121c388806513913265f5
Klara <kucinska.klara@gmail.com>
2018-07-02 16:29:23 +0200

    szachownica

--- task10.php
+++ task10.php
@@ -1,12 +1,16 @@
+<table>
 <?php
-//$a= array[20][20];
 $abecadlo=['A', 'B','C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T'];
 
 for($i=0; $i<20; $i++){
+	?><tr><?php
 	for($j=0; $j<20; $j++){
 		$a[$i][$j]= $i.$abecadlo[$j];
-		echo $a[$i][$j];
-		echo ' ';
+		?><th style="border-style: solid; border-width: 5px; width: 20px;"><?php
+		echo $a[$i][$j];?></th><?php
 	}
-	echo '<br>';
+	?></tr><?php
 }
+?>
+</table>
+<?php
