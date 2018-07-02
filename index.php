9b46ed00c4471f3d1ad75346a2633f02b48ded8f
Oleksii Fursov <nodePro777@gmail.com>
2018-07-02 14:15:25 +0200

    Yeah

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+
+
+
+echo 'Uczestniczę w "PHP CAMP" #2 <br/> 
+Na $BANK się czegoś nauczę! <br/>
+Jak to usunąłeś C:\\moje_firmy? <br/>
+Szklanka jest w 1/n pełna <br/>
+Dla n = 2 szklanka jest w <sup>1</sup>/<sub>2</sub> pełna <br/>4
+I\'m a little mama\'s boy <br/><br/>',
+
+"Uczestniczę w \"PHP CAMP\" #2 <br/> \n\t 
+Na \$BANK się czegoś nauczę! <br/>\n\t 
+Jak to usunąłeś C:\\\\moje_firmy? <br/>\n\t 
+Szklanka jest w 1/n pełna <br/>\n\t 
+Dla n = 2 szklanka jest w <sup>1</sup>/<sub>2</sub> pełna <br/>4\n\t 
+I'm a little mama's boy <br/>";
+
+
+
