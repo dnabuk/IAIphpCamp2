a36e89b133735610d29af20c680b639bb44d1803
a-Zszywacz <Teresher935@GMail.com>
2018-07-02 15:41:03 +0200

    tables/arrays

---
+++ index4.php
@@ -0,0 +1,18 @@
+<?php
+$camp = array([1 => "Patryk", '1' => "Chowratowicz", 1.99 => "Choszczno", true => "Hahaha"]);
+var_dump($camp);
+$camp[4]= 34;
+echo "<br>";
+var_dump($camp);
+$camp[]= "Kot ma Ale";
+echo "<br>";
+var_dump($camp);
+unset($camp[4]);
+echo "<br>";
+var_dump($camp);
+$camp[]= "Ala tez zawsze ma zawsze 'jakies ale'";
+echo "<br>";
+var_dump($camp);
+$camp[]=array(range(1, 5));
+echo "<br>";
+var_dump($camp);
