9f85ef24744b8aa8656eea7de1d035ef8f6d8500
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-07-02 17:59:02 +0200

    from home

--- TabZad.php
+++ TabZad.php
@@ -1,24 +1,10 @@
 <?php
-$tabLetter = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T');
-$tab[20][20] = array();
-for ($i = 0; $i < 20; $i++){
-    foreach ($tabLetter as $value){
-        echo $i . $value;
-        if ($i == 20){
-            echo '<br>';
-        }
-    }
-
-
-    for ($j = 0; $j <= 20; $j++){
-        foreach ($tabLetter as $value) {
-            echo $j . $value;
-            if ($j == 20){
-                echo '<br>';
-            }
-        }
-
-
-    }
+$numbersTable = range(0, 19);
+$lettersTable = range('A', 'T');
+foreach ($numbersTable as $number){
+    echo "<div>";
+    foreach ($lettersTable as $letters) {
+        echo $number.$letters;
+    };
+    echo "</div>";
 }
-//getcolor=1, getname=michal, gethard
