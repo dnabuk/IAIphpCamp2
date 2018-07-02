1c280ff09e37758e3054503f5586bdc473394413
jPrzeworski <jakub.przeworski@onet.com.pl>
2018-07-02 16:25:40 +0200

    Update

--- plik3.php
+++ plik3.php
@@ -1,15 +1,24 @@
 <?php
 
-$camp = array(
-	1 => 'Jakub', 
-	'1' => 'Przeworski', 
-	1.99 => 'Szczecin', 
-	true => 'Gniewko', 
-	4 => 'klucz4', 
-	'Kot ma Alê', 
-	'Ala te¿ zawsze ma zawsze \'jakieœ ale\'',
-	array(1,2,3,4,5)
-	);
-	
-unset($camp[4]);
+$litery = range('A','T');
+
+foreach($litery as $j)
+{
+	for($i=0; $i<20; $i++)
+	{
+		if($i % 2 == 0)
+			$color = 'yellow';
+		else
+			$color = 'red';
+		echo "<div style='display: inline-block;
+		background-color: ".$color.";
+		width: 50px;
+		height: 50px;
+		border: solid 1px black;'>". $i . $j."</div>";
+		if($i == 19)
+			echo "<br>";
+		
+			
+	}	
+}
 ?>
