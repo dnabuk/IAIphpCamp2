3991dbfef934a1424a54032db7ee41c34e807320
a-Zszywacz <Teresher935@GMail.com>
2018-07-02 14:15:06 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    $io;
+    echo 'Uczestniczę w "PHP CAMP" #2 '.'<br>'.
+        'Na $BANK się czegoś nauczę!'.'<br>'.
+    'Jak to usunąłeś C:\\\\moje_filmy?'.'<br>'.
+        'Szklanka jest w 1/n pełna'.'<br>'.
+    'Dla n = 2 szklanka jest w &#189 pełna'.'<br>'.
+    'I\'m a little mama\'s boy';
+echo "<br>";
+echo "<br>";
+echo "<br>";
+echo "Uczestniczę w \"PHP CAMP\" #2 "."<br>".
+    "Na \$BANK się czegoś nauczę!"."<br>".
+    "Jak to usunąłeś C:\\\\moje_filmy?"."<br>".
+    "Szklanka jest w 1/n pełna"."<br>".
+    "Dla n = 2 szklanka jest w &#189 pełna"."<br>".
+    "I'm a little mama's boy";
+?>
