74dc53266fc666a27483c357e109f4d63e7ad133
Becia <becia09269@wp.pl>
2018-07-02 15:42:14 +0200

    tablicaCd

--- tablice.php
+++ tablice.php
@@ -8,5 +8,31 @@ true => "Gniewomir"
 );
 
 var_dump ($camp);
+echo "<br>";
+
+$camp[4] = "wartosc";
+
+var_dump ($camp);
+echo "<br>";
+
+$camp[5] = "Kot ma Alę";
+
+var_dump ($camp);
+echo "<br>";
+
+unset ($camp[4]);
+
+var_dump ($camp);
+echo "<br>";
+
+$camp[6] = 'Ala zawsze ma \'jakieś ale';
+
+var_dump ($camp);
+echo "<br>";
+
+$array2 = 5;
+$camp[6] = $array2;
+
+var_dump ($camp);
 
 ?>
