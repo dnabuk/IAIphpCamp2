5ae069a103dead105f5d9b89ca7fc62fe00e0d49
Klara <kucinska.klara@gmail.com>
2018-07-02 14:47:18 +0200

    no message

---
+++ task2.php
@@ -0,0 +1,13 @@
+<?php
+echo 1+'1'.'<br>';
+echo 1+'-1.1'.'<br>';
+echo 1+'-0.3e3'.'<br>';
+echo 1+'30 uczestnikow'.'<br>';
+echo 1+'#2 PHP Camp'.'<br><br>';
+echo '1'+1;
+echo '<br>';
+echo ((1.1)+1);
+echo '<br>';
+echo (1+(1.1));
+echo '<br>';
+//echo (1..1+1);
