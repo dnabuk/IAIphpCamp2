a2e685d6cd2f212d4ab68e7e712cab49c6b56a31
Bartlomiej Karasiewicz <bkrswcz@gmail.com>
2018-07-02 15:32:29 +0200

    zmiany

--- newfile.php
+++ newfile.php
@@ -1,22 +1,27 @@
 ﻿<?php
-echo 'Uczestniczę w /"PHP CAMP/" #2'
-?>
-
-Uczestniczę w "PHP CAMP" #2
-Na $BANK się czegoś nauczę!
+echo 'Uczestniczę w "PHP CAMP" #2';
+/*echo 'Na $BANK się czegoś nauczę!'
 Jak to usunąłeś C:\\moje_filmy?
 Szklanka jest w 1\n pełna
 Dla n = 2 szklanka jest w ½ pełna
-I'm a little mama's boy
+I'm a little mama's boy*/
 
 
-1 + '1'
-1 + '-1.1'
+echo "1 + '1'" ;
+/*1 + '-1.1'
 1 + '-0.3e3'
 1 + '30 uczestników'
 1 + '#2 PHP Camp'
 '1' + 1
 1.1 + 1
 1 + 1.1
-1..1 + 1
+1..1 + 1*/
+
+//$array
+
+$camp = array(1=>'Bartłomiej','1'=>'Karasiewicz',1.99=>'Płock',TRUE='Gniewko')
+echo $camp
+
+
+?>
 
