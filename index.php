38da217de273b8cfc241618c0e4682466c912334
Pawel Lica <pawel.lica@gmail.com>
2018-07-02 14:26:28 +0200

    Utworzone

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ echo '<br>Na $BANK się czegoś nauczę!';
 echo '<br>Jak to usunąłeś C:\\\\moje_filmy?';
 echo '<br>Szjkabja hest w 1/n pełna';
 echo '<br>Dla n=2 szklanka jest w ½';
-echo '<br>I\'m a little mama\'s boy';
+echo '<br>I\'m a little mama\'s boy<br><br>';
 
 echo "Uczestniczę w \"PHP CAMP\" #2";
 echo "<br>Na \$BANK się czegoś nauczę!";
