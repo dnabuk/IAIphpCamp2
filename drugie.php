8036bd3024ea08e914992ee9049fe036a4b5df16
Marta Jaworska <maartajaworska@gmail.com>
2018-07-02 15:39:10 +0200

    zadanie drugie czesc druga

--- drugie.php
+++ drugie.php
@@ -5,6 +5,24 @@
 					'1' => "Jaworska",
 					1.99 => "Rzeszów",
 					true => "Gniewko");
+					
+	echo "<br/>";
+	var_dump($camp);
+	$camp[4] = "nowy element";
+	echo "<br/>";
+	var_dump($camp);
+	$camp[] = "Kot ma Alę";
+	echo "<br/>";
+	var_dump($camp);
+	unset($camp[4]);
+	echo "<br/>";
+	var_dump($camp);
+	$camp[] = "Ala też ma zawsze 'jakies ale'";
+	echo "<br/>";
+	var_dump($camp);
+	$tablica_intow = [1,2,3,4,5];
+	$camp[] = $tablica_intow;
+	echo "<br/>";
 	var_dump($camp);
 ?>
 </body>
