f72ba3473ae67f1880323e6719927b54b651036c
Klara <kucinska.klara@gmail.com>
2018-07-02 16:36:43 +0200

    dodanie kolorów szachownicy

--- task10.php
+++ task10.php
@@ -6,7 +6,11 @@ for($i=0; $i<20; $i++){
 	?><tr><?php
 	for($j=0; $j<20; $j++){
 		$a[$i][$j]= $i.$abecadlo[$j];
-		?><th style="border-style: solid; border-width: 5px; width: 20px;"><?php
+		if($i%2==0 && $j%2==0 || $i%2==1 && $j%2==1){
+			?><th style="background-color: yellow; border-style: solid; border-width: 1px; width: 20px;"><?php
+		}elseif($i%2==1 && $j%2==0 || $i%2==0 && $j%2==1){
+			?><th style="background-color: brown; border-style: solid; border-width: 1px; width: 20px;"><?php
+		}
 		echo $a[$i][$j];?></th><?php
 	}
 	?></tr><?php
