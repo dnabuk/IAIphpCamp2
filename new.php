7b100656e555afff94837811adf7d5138f0dcc20
Dagmara <dagaxdd@hotmail.com>
2018-07-02 15:55:25 +0200

    zadanie

---
+++ new.php
@@ -0,0 +1,15 @@
+<?php
+$camp=[];
+$camp[1]="Dagmara";
+$camp['1']="Tęga";
+$camp[1.99]="Szczecin";
+$camp[true]=Gniewomir;
+var_dump($camp);
+$camp[]="Kot ma Alę";
+var_dump($camp);
+unset($camp[5]);
+var_dump($camp);
+$camp[]="Ala też zawsze ma zawsze 'jakieś ale'";
+var_dump($camp);
+$tablica[]=array(1,2,3,4,5);
+var_dump($camp);
