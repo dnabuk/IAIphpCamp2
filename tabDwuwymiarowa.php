62d733eed1f80dd69377259e41cca0d32626aa2d
Becia <becia09269@wp.pl>
2018-07-02 16:30:01 +0200

    szachownica-siatka

--- tabDwuwymiarowa.php
+++ tabDwuwymiarowa.php
@@ -1,14 +1,14 @@
 <?php
 
+echo '<table border = "1" cellpadding = "10" cellspacing = "0">';
 foreach (range(0, 19) as $liczba) {
 
+	echo "<tr>";
 	foreach (range('A', 'T') as $litera)
 	{
-		echo $liczba;
-		echo $litera."  ";
+		echo "<td>".$liczba.$litera."</td>";
 	}
-	echo "<br>";
-
+	echo "<tr/>";
 }
 
 ?>
