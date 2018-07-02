bad0a769b6914aba8ab27d847c4daffa1e730e01
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-02 14:10:33 +0200

    Zrobione zadanie task1

--- task1.php
+++ task1.php
@@ -1,3 +1,15 @@
 <?php
-	
+	echo 'Uczestniczę w "PHP Camp"#2<br>';
+	echo 'Na $BANK się czegoś nauczę!<br>';
+	echo 'Jak to usunąłeś C:\\\\moje_filmy?<br>';
+	echo 'Szklanka jest w 1/n pełna<br>';
+	echo 'Dla n = 2 szklanka jest w &#189 pełna<br>';
+	echo 'I\'m a little mama\'s boy<br>';
+	echo '<br>';
+	echo "Uczestniczę w \"PHP Camp\"#2<br>";
+	echo "Na \$BANK się czegoś nauczę!<br>";
+	echo "Jak to usunąłeś C:\\\\moje_filmy?<br>";
+	echo "Szklanka jest w 1/n pełna<br>";
+	echo "Dla n = 2 szklanka jest w \u{00BD} pełna<br>";
+	echo "I'm a little mama's boy<br>";
 ?>
