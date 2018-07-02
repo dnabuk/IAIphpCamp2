9b46c2ffe8ad9a8e6977b2995e71efc1791c0585
Pawel Lica <pawel.lica@gmail.com>
2018-07-02 16:24:15 +0200

    Kolory

--- zadanie5.php
+++ zadanie5.php
@@ -3,12 +3,17 @@ $tab1=range(0, 19, 1);
 $tab2=range('A', 'T', 1);
 $dziwnaTablica = [$tab1, $tab2];
 echo '<table>';
-
+$i=0;
 foreach ($dziwnaTablica[0] as $valX){
 	echo '<tr>';
 	foreach ($dziwnaTablica[1] as $valY){
-		echo '<td>'.$valX.$valY.'</td>';
-		
+		if ($i%2==0){
+			echo '<td bgcolor=brown>'.$valX.$valY.'</td>';
+		} else {
+			echo '<td bgcolor=yellow>'.$valX.$valY.'</td>';
+		}
+		$i++;
 	}
+	$i++;
 	echo '</tr>';
 }
