12bdd39bb013e2ec062bb257320d2d98e51d0741
Kacper Lisicki <lisicki.biznes@gmail.com>
2018-07-02 16:38:17 +0200

    taks4

--- taks4.php
+++ taks4.php
@@ -5,7 +5,7 @@ $t2 = range('A', 'T');
 foreach ($t1 as $a){
     echo "<div>";
     foreach ($t2 as $b) {
-            if ($a % 2 == 0) {
+            if ($a % 2 == 0 %% ) {
                 echo '<div style="display: inline-block; background-color:white; width: 50px; height: 50px; border: solid 1px black;">';
             } else {
                 echo '<div style="display: inline-block; background-color:red; width: 50px; height: 50px; border: solid 1px black;">';
