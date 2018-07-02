ba3ae8b233b115cb41a0320fc35debe353b6ec3e
Dagmara <dagaxdd@hotmail.com>
2018-07-02 14:22:22 +0200

    zmiana w index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+<?php
+echo 'Uczestniczę w "PHP CAMP" #2';
+echo "</br>";
+echo 'Na $BANK się czegoś nauczę!';
+echo "</br>";
+echo 'Jak to usunąłeś C:\\moje_filmy?';
+echo "</br>";
+echo 'Szklanka jest w 1/n pełna';
+echo "</br>";
+echo 'Dla n = 2 szklanka jest w ½ pełna';
+echo "</br>";
+echo "I'm a little mama's boy";
