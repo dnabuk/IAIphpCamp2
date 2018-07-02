ba2fc8b4f5f5af6d184d83a8af96339f8c433382
Adrian Pabisiak <adrianpabisiak2@gmail.com>
2018-07-02 14:27:24 +0200

    tekst

---
+++ zad.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+$a="#BANK";
+
+echo 'Uczestnicze w "PHP CAMP" #2 <br/>'; 
+echo "Na ".$a." sie czegos naucze!<br />";
+echo "jak to usunales C:\\\MOJE_FILMY?<br/>";
+echo "szklanka jest w 1/n pelna<br/>"; 
+echo "dla n=2 szklanka jest w 1/2 pelna <br/>";
+echo "I`m a little mama`s boy";
+?>
+</body>
+</html>
