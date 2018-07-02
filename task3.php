266255db7d0e9883b1048f1f6f63e58e860020ca
Piotr <pittero1@wp.pl>
2018-07-02 15:47:13 +0200

    zadanie nr 4

--- task3.php
+++ task3.php
@@ -1,9 +1,18 @@
 <?php
 $camp = array(
-1 => 'Piotr',
-'1' => 'Żerański',
-1.99 => 'Nie pije:)',
-true => 'Gniewomir'
+	1 => 'Piotr',
+	'1' => 'Żerański',
+	1.99 => 'Nie pije:)',
+	true => 'Gniewomir'
 );
+$camp[4] = 'Nowy element';
+var_dump($camp);
+$camp[] = 'Kot ma Alę';
+var_dump($camp);
+unset($camp[4]);
+var_dump($camp);
+$camp[] = 'Ala też zawsze ma zawsze \'jakieś ale\'';
+var_dump($camp);
+$camp[] = array(range(1,5));
 var_dump($camp);
 ?>
