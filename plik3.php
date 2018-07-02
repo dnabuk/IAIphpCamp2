67580f3f2ed27c1d36c5666b6b2a382d6ce81332
jPrzeworski <jakub.przeworski@onet.com.pl>
2018-07-02 16:37:25 +0200

    Finish colors

--- plik3.php
+++ plik3.php
@@ -2,23 +2,35 @@
 
 $litery = range('A','T');
 
-foreach($litery as $j)
+for($i=0; $i<20; $i++)
 {
-	for($i=0; $i<20; $i++)
+	$k = 0;
+	foreach($litery as $j)
 	{
 		if($i % 2 == 0)
-			$color = 'yellow';
+		{
+			if($k % 2 == 0)
+				$color = 'yellow';
+			else
+				$color = 'red';
+		}
 		else
-			$color = 'red';
+		{
+			if($k % 2 != 0)
+				$color = 'yellow';
+			else
+				$color = 'red';
+		}
+		
 		echo "<div style='display: inline-block;
 		background-color: ".$color.";
 		width: 50px;
 		height: 50px;
 		border: solid 1px black;'>". $i . $j."</div>";
-		if($i == 19)
+		if($j == 'T')
 			echo "<br>";
 		
-			
+		$k++;
 	}	
 }
 ?>
