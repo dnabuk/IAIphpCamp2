f503cba483f65fd6af624353665379bf5e36e7a6
Daniel Molenda <danielmolenda16@gmail.com>
2018-07-02 14:26:49 +0200

    task1

---
+++ cw.php
@@ -0,0 +1,15 @@
+<?php
+    echo "Uczestniczę w \"PHP CAMP\" #2 <br>";
+    echo "Na \$BANK się czegoś uczę!<br>";
+    echo "Jak to usunąłeś C:\\\mojefilmy?<br>";
+    echo "Szklanka jest 1/n pełna<br>";
+    echo "Dla n=2 szklanka jest 1/2 pełna<br>"; 
+    echo "I'm a little mama's boy<br><br>";
+    echo 'Uczestniczę w "PHP CAMP" #2<br>';
+    echo 'Na $BANK się czegoś uczę!<br>';
+    echo 'Jak to usunąłeś C:\\mojefilmy?<br>';
+    echo 'Szklanka jest 1/n pełna<br>';
+    echo 'Dla n=2 szklanka jest 1/2 pełna<br>';
+    echo 'I\'m a little mama\'s boy';
+
+?>
