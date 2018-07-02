3ec0e57ec8c02d8825cdc6aa026397804ebc4f1c
Krzysztof Margański <bystek@o2.pl>
2018-07-02 15:43:00 +0100

    tablica ciąg dalszy

--- task3.php
+++ task3.php
@@ -7,3 +7,12 @@ true=>'Gniewosz'
 ];
 
 var_dump($camp);
+$camp[4]='';
+var_dump($camp);
+$camp[]='Kot ma Alę';
+unset($camp[4]);
+var_dump($camp);
+$camp[]='Ala też zawsze ma zawsze \'jakieś ale';
+var_dump($camp);
+$camp[]=array(1,2,3,4,5);
+var_dump($camp);
