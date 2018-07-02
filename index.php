8bdc6b5f388f0047fa21c409cff83af6e2c0c60d
Becia <becia09269@wp.pl>
2018-07-02 14:27:07 +0200

    stringi

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 
 echo "Uczestniczę w \"PHP CAMP\" #2 <br>
 Na \$BANK się czegoś nauczę! <br>
-Jak to usunąłeś C:\\moje_filmy? <br>
+Jak to usunąłeś C:\\\moje_filmy? <br>
 Szklanka jest w 1\\n pełna <br>
-Dla n = 2 szklanka jest w &frac12 pełna <br>
+Dla n = 2 szklanka jest w ½ pełna <br>
 I'm a little mama's boy <br> <br> <br>";
 
 
 echo 'Uczestniczę w "PHP CAMP" #2 <br>
 Na $BANK się czegoś nauczę! <br>
-Jak to usunąłeś C:\\moje_filmy? <br>
+Jak to usunąłeś C:\\\moje_filmy? <br>
 Szklanka jest w 1\n pełna <br>
 Dla n = 2 szklanka jest w &frac12 pełna <br>
 I\'m a little mama\'s boy <br>';
