90f4595849e8079e2c0ab056ca54f938b0b726ba
Piotr <pittero1@wp.pl>
2018-07-02 15:52:16 +0200

    zadanie nr 4-poprawka array5

--- task3.php
+++ task3.php
@@ -9,7 +9,7 @@ $camp[4] = 'Nowy element';
 var_dump($camp);
 $camp[] = 'Kot ma Alę';
 var_dump($camp);
-unset($camp[4]);
+unset($camp[5]);
 var_dump($camp);
 $camp[] = 'Ala też zawsze ma zawsze \'jakieś ale\'';
 var_dump($camp);
