ebdc1376ff8061b046d49cd29445530a27e39d5a
Oleksii Fursov <nodePro777@gmail.com>
2018-07-02 16:11:36 +0200

    Yeah #7

---
+++ task4.php
@@ -0,0 +1,37 @@
+<style>
+table{
+	border-collapse: collapse;
+}
+td{
+	width: 50px; height: 50px; border: solid 1px #000; vertical-align: top;
+}
+</style>
+<?php
+
+
+
+function gen_data($sizeX=20, $sizeY=20){
+	$arr = array();
+	$startA = ord('A');
+	for($i=0; $i<$sizeY; $i++){
+		$arr[$i] = array();
+		for($j=0; $j<$sizeX; $j++){
+			$arr[$i][$j] = $i.chr($startA+$j);
+		}
+	}
+	return $arr;
+}
+function create_table($data){
+	$buf = '<table border="1"><tbody>';
+	for($i=0, $leni=sizeof($data); $i<$leni; $i++){
+		$buf.='<tr>';
+		for($j=0, $lenj=sizeof($data[$i]); $j<$lenj; $j++){
+			$buf.='<td>'.$data[$i][$j].'</td>';
+		}
+		$buf.='</tr>';
+	}
+	return $buf.'</table>';
+}
+echo create_table(gen_data(20,20));
+
+?>
