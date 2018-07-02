21dc50a3769a4e70f5a318e407ab2326db0919d2
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2018-07-02 15:53:08 +0200

    arrays remove key 4

--- plik3.php
+++ plik3.php
@@ -1,18 +1,51 @@
 <?php
 
 
-$camp = array(
+$camp1 = array(
   1 => "Patrycjusz",
   '1' => "Nowaczyk",
   1.99 => "Poznań",
-  1 => 'Gniewko'
+  1 => 'Gniewko',
+  4 => "Random value",
 );
+var_dump($camp1);
+echo "</br></br>";
 
-foreach ($camp as $key => $item) {
-  echo "$item\n";
-  //reset($camp);
-}
+$camp2 = array(
+  1 => "Patrycjusz",
+  '1' => "Nowaczyk",
+  1.99 => "Poznań",
+  1 => 'Gniewko',
+  4 => "Random value",
+  5 => "Kot ma Alę"
+);
+var_dump($camp2);
 
+unset($camp2[4]);
+echo "</br>";
+var_dump($camp2);
 
+echo "</br></br>";
+echo "</br></br>";
+
+$camp3 = array(
+  1 => "Patrycjusz",
+  '1' => "Nowaczyk",
+  1.99 => "Poznań",
+  1 => 'Gniewko',
+  4 => "Random value",
+);
+var_dump($camp3);
+echo "</br></br>";
+
+$camp4 = array(
+  1 => "Patrycjusz",
+  '1' => "Nowaczyk",
+  1.99 => "Poznań",
+  1 => 'Gniewko',
+  4 => "Random value",
+  5 => "Kot ma Alę"
+);
+var_dump($camp4);
 
  ?>
