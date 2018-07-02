d3a54168f6b703412d39502401542301c60abd21
Gringeril <gabisska@gmail.com>
2018-07-02 14:16:12 +0200

    Task1

---
+++ test.php
@@ -0,0 +1,17 @@
+<?php
+
+echo 'Uczestniczę w "PHP CAMP" #2 <br/>';
+echo 'Na $BANK się czegoś nauczę! <br/>';
+echo 'Jak to usunąłeś C:\\\moje_filmy? <br/>';
+echo 'Szklanka jest w 1\n pełna <br/>';
+echo 'Dla n = 2 szklanka jest w ½ pełna <br/>';
+echo 'I\'m a little mama\'s boy <br/><br/>';
+
+echo "Uczestniczę w \"PHP CAMP\" #2 <br/>";
+echo "Na \$BANK się czegoś nauczę! <br/>";
+echo "Jak to usunąłeś C:\\\moje_filmy? <br/>";
+echo "Szklanka jest w 1\n pełna <br/>";
+echo "Dla n = 2 szklanka jest w ½ pełna <br/>";
+echo "I'm a little mama's boy <br/><br/>";
+
+?>
