b3c8e56cddb1ef88e70447a18f36fb3677109489
Marta Jaworska <maartajaworska@gmail.com>
2018-07-02 23:33:37 +0200

    zadanie 3 kolory 2 czesc

--- trzecie_2.php
+++ trzecie_2.php
@@ -1,35 +1,25 @@
-<html>
-<body>
+
 <?php 
 	$jaki = 'style = background-color : yellow';
 	$jaki2 = 'style = background-color : red';
 	$litery = range("A", "T");	
 	$liczba = range(0,19);
-	$kolor = 1;
+	
 	echo "<br/>";
-	//var_dump($camp);
-	//print_r($litery);
-	//print_r($liczba);
 	$zmienna = "<table>";
 	foreach($liczba as $klucz => $element)
 	{
 		$zmienna .= "<tr>";
 		foreach($litery as $k => $e)
 		{
-			if($kolor == 1) $zmienna .= "<td style = \"background-color: red\">";
+			if($k > 19-$element) $zmienna .= "<td style = \"background-color: red\">";
 			else $zmienna .= "<td style = \"background-color: yellow\">";
 			$zmienna .= $element.$e;
 			$zmienna .= "</td>";
-			if($kolor == 1) $kolor =0;
-			else $kolor = 1;
 		}
 		$zmienna .= "</tr>";
-		if($kolor == 1) $kolor =0;
-			else $kolor = 1;
 	}
 	$zmienna .= "</table>";
 	echo $zmienna;
 
 ?>
-</body>
-</html>
