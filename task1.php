16e30495bbe35657ecedda1737bf88f3398a4b58
Krzysztof Margański <bystek@o2.pl>
2018-07-02 14:20:00 +0100

    Pierwsza zmiana

---
+++ task1.php
@@ -0,0 +1,30 @@
+<?php
+echo 'Uczestniczę w "PHP CAMP" #2
+Na $BANK się czegoś nauczę!
+Jak to usunąłeś C:\\moje_filmy?
+Szklanka jest w 1\n pełna
+Dla n = 2 szklanka jest w ½ pełna
+I\'m a little mama\'s boy';
+echo '<br>';
+echo "Uczestniczę w \"PHP CAMP\" #2
+Na \$BANK się czegoś nauczę!
+Jak to usunąłeś C:\\moje_filmy?
+Szklanka jest w 1\\n pełna
+Dla n = 2 szklanka jest w ½ pełna
+I'm a little mama's boy";
+echo '<br>';
+if($_GET['hint']==1){
+	echo 'echo \'Uczestniczę w "PHP CAMP" #2
+Na $BANK się czegoś nauczę!
+Jak to usunąłeś C:\\moje_filmy?
+Szklanka jest w 1\n pełna
+Dla n = 2 szklanka jest w ½ pełna I\'m a little mama\'s boy\';';
+echo '<br>';
+echo 'echo "Uczestniczę w \"PHP CAMP\" #2
+Na \$BANK się czegoś nauczę!
+Jak to usunąłeś C:\\moje_filmy?
+Szklanka jest w 1\n pełna
+Dla n = 2 szklanka jest w ½ pełna
+I\'m a little mama\'s boy";';
+
+}
