40b02304eeaf91b1c716976e78ebc72d6edb7355
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2018-07-02 15:33:47 +0200

    arrays first exercise

---
+++ plik3.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$camp = array(
+  1 => "Patrycjusz",
+  '1' => "Nowaczyk",
+  1.99 => "Poznań",
+  1 => 'Gniewko'
+);
+
+foreach ($camp as $key => $item) {
+  echo "$item\n";
+  //reset($camp);
+}
+
+
+
+ ?>
