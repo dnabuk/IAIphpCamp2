100d24a3462e39728d1aa577729e0fcf1e860f7b
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-02 16:35:02 +0200

    Do dokończenia zmiana kolorowania w task10.php

--- task10.php
+++ task10.php
@@ -2,19 +2,22 @@
 	$tablica = array();
 	$color1 = 'style="background-color: yellow"';
 	$color2 = 'style="background-color: brown"';
-	
-	
+		
 	echo "<table border=1>";
 	foreach (range(1, 20) as $col) {
 		echo "<tr>";
 		foreach (range('A', 'T') as $row => $val) {
 			$tablica[$col][$row] = $col . $val;
-			if (($col + $row) % 2 == 0) {
+			/*if (($col + $row) % 2 == 0) {
 				$color = $color1;
 			}
 			else {
 				$color = $color2;
+			}*/
+			if () {
+				
 			}
+						
 			echo "<td " . $color . ">" . $tablica[$col][$row] . "</td>";
 		}
 		echo "</tr>";
