5eda8a5f0f313f03eda7013b1a8ed5427a1218fe
Daniel Molenda <danielmolenda16@gmail.com>
2018-07-02 16:36:57 +0200

    tablica zrobiona

--- task10.php
+++ task10.php
@@ -4,7 +4,7 @@
     {
         foreach($litery as $j)
         {
-            echo '<div style="display: inline-block; background-color:white; width: 50px; height: 50px; border: solid 1px black;">'.$i.$j.'</div><br>';
+            echo '<div style="display: inline-block; background-color:white; width: 50px; height: 50px; border: solid 1px black;">'.$i.$j.'</div>';
             if($j == "T")
                 echo "<br>";
         }
