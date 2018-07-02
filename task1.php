a403a688fc1021e357f85d3f05837f51697f4e4c
Klara <kucinska.klara@gmail.com>
2018-07-02 14:17:40 +0200

    wyświetlanie stringów

--- task1.php
+++ task1.php
@@ -1,4 +1,8 @@
 <?php
-
-echo 'Hello';
+echo "Uczestniczę w "; 
+echo '"PHP CAMP"'; 
+echo " #2 Na"; 
+echo ' $BANK'; 
+echo "\n";
+echo " się czegoś nauczę! \n Jak to usunąłeś C:\\\moje_filmy? \n Szklanka jest w 1/n pełna \n Dla n=2 szkalnka jest w 1/2 pełna \n I'm a little mama's boy";
 
