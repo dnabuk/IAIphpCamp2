a1a03c6b1e5946fc9c7245ce12b4bb6c191a424f
Pawel Lica <pawel.lica@gmail.com>
2018-07-02 22:02:28 +0200

    Romb

--- zadanie5.php
+++ zadanie5.php
@@ -1,8 +1,21 @@
 <?php
-$tab1=range(0, 19, 1);
-$tab2=range('A', 'T', 1);
+$mX=19;
+$mY='T';
+
+if (isset($_GET['X'])){
+	$mX=$_GET['X'];
+}
+if (isset($_GET['Y'])){
+	$mY=$_GET['Y'];
+}
+$tab1=range(0, $mX, 1);
+$tab2=range('A', $mY, 1);
 $dziwnaTablica = [$tab1, $tab2];
 echo '<table>';
+
+
+$rX = count($dziwnaTablica[0]);
+$rY = count($dziwnaTablica[1]);
 
 $j=0;
 foreach ($dziwnaTablica[0] as $valX){
@@ -10,10 +23,13 @@ foreach ($dziwnaTablica[0] as $valX){
 	$i=0;
 	foreach ($dziwnaTablica[1] as $valY){
 		
-		if ($i+$j<count($dziwnaTablica[0])/2){
-			echo '<td bgcolor=brown>'.$valX.$valY.'</td>';
-		} else {
+		if (($i-$j>=$rX/2)||
+		   (($i+$j-$rX/2)+1<=0)||
+		   (($i-$j)+$rX/2<=0)||
+		   (($i+$j-$rX+1)>=$rX/2)){
 			echo '<td bgcolor=yellow>'.$valX.$valY.'</td>';
+		} else {
+			echo '<td bgcolor=brown>'.$valX.$valY.'</td>';
 		}
 		$i++;
 	}
