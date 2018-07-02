7c6074769f16d66f2a634ba0a24beb0ec56d04e0
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-02 16:08:27 +0200

    Utworzenie siatki z wartościami w task10.php

--- task10.php
+++ task10.php
@@ -1,3 +1,16 @@
 <?php
-
+	$tablica = array();
+	
+	echo "<table border=1>";
+	foreach (range(1, 20) as $col) {
+		echo "<tr>";
+		foreach (range('A', 'T') as $row => $val) {
+			$tablica[$col][$row] = $col . $val;
+			echo "<td>" . $tablica[$col][$row] . "</td>";
+		}
+		echo "</tr>";
+	}
+	echo "</table>";
+	
+	
 ?>
