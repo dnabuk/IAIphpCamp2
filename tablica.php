69406d188b423ada495ba28b47139a98b5cb243f
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-02 15:36:28 +0200

    Kolejna edycja tablica.php

--- tablica.php
+++ tablica.php
@@ -5,4 +5,19 @@
 	$camp[1.99] = "Poznań";
 	$camp[true] = "Gniewomir";
 	var_dump($camp);
+	echo "<br>";
+	$camp[4] = 4;
+	var_dump($camp);
+	echo "<br>";
+	$camp[] = "Kot ma Alę";
+	var_dump($camp);
+	echo "<br>";
+	unset($camp[4]);
+	var_dump($camp);
+	echo "<br>";
+	$camp[] = "Ala też zawsze ma zawsze 'jakieś ale'";
+	var_dump($camp);
+	echo "<br>";
+	$camp[] = array(1, 2, 3, 4, 5);
+	var_dump($camp);
 ?>
