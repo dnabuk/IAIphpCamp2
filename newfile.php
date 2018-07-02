66e0631557a5d9f563981937989461b768fcd098
Bartlomiej Karasiewicz <bkrswcz@gmail.com>
2018-07-02 14:50:00 +0200

    zmiany

---
+++ newfile.php
@@ -0,0 +1,22 @@
+﻿<?php
+echo 'Uczestniczę w /"PHP CAMP/" #2'
+?>
+
+Uczestniczę w "PHP CAMP" #2
+Na $BANK się czegoś nauczę!
+Jak to usunąłeś C:\\moje_filmy?
+Szklanka jest w 1\n pełna
+Dla n = 2 szklanka jest w ½ pełna
+I'm a little mama's boy
+
+
+1 + '1'
+1 + '-1.1'
+1 + '-0.3e3'
+1 + '30 uczestników'
+1 + '#2 PHP Camp'
+'1' + 1
+1.1 + 1
+1 + 1.1
+1..1 + 1
+
