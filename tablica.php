50345428aa0c069dd3675f5e293bb5a0807624b6
Daniel Molenda <danielmolenda16@gmail.com>
2018-07-02 16:03:28 +0200

    tablice

--- tablica.php
+++ tablica.php
@@ -1,6 +1,14 @@
 <?php
-    $camp = array(1=>"Daniel", "1"=>"Molenda", 1.99=>"Szczecin", true=>"Gniewomir");
+    $camp[1] = "Daniel";
+    $camp["1"] ="Molenda";
+    $camp[1.99] = "Szczecin";
+    $camp[true] = "Gniewomir";
+    $camp[4] = "Ala ma kota";
+    $camp[]="Kot ma Alę";
+    $camp[]='Ala też zawsze ma zawsze \'jakieś ale\'';
+    $camp[] = [1,2,3,4,5];
+
+    unset($camp[5]);
 
     echo $camp[1];
-
 ?>
