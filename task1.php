fca181a902fa7d9b87618df177bcbe2ec9f43894
Piotr <pittero1@wp.pl>
2018-07-02 14:24:27 +0200

    zadanie nr 1

---
+++ task1.php
@@ -0,0 +1,29 @@
+﻿<?php
+echo 'Uczestniczę w "PHP CAMP" #2';
+echo '<br/>';
+echo 'Na $BANK się czegoś nauczę!';
+echo '<br/>';
+echo 'Jak to usunąłeś C:\\\\moje_filmy?';
+echo '<br/>';
+echo 'Szklanka jest w 1h pełna';
+echo '<br/>';
+echo 'Dla n = 2 szklanka jest w ½ pełna';
+echo '<br/>';
+echo 'I\'m a little mama\'s boy';
+
+echo '<br/>';
+echo '<br/>';
+
+echo "Uczestniczę w \"PHP CAMP\" #2";
+echo "<br/>";
+echo "Na \$BANK się czegoś nauczę!";
+echo "<br/>";
+echo "Jak to usunąłeś C:\\\moje_filmy?";
+echo "<br/>";
+echo "Szklanka jest w 1h pełna";
+echo "<br/>";
+echo "Dla n = 2 szklanka jest w ½ pełna";
+echo "<br/>";
+echo "I'm a little mama's boy";
+
+?>
