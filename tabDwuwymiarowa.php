8b135ce93ca5befba05024ad57303d065817434b
Becia <becia09269@wp.pl>
2018-07-02 16:36:09 +0200

    szachownica-kolory

--- tabDwuwymiarowa.php
+++ tabDwuwymiarowa.php
@@ -1,12 +1,23 @@
 <?php
 
+$licznik = 0;
+
 echo '<table border = "1" cellpadding = "10" cellspacing = "0">';
+
 foreach (range(0, 19) as $liczba) {
 
 	echo "<tr>";
 	foreach (range('A', 'T') as $litera)
 	{
-		echo "<td>".$liczba.$litera."</td>";
+		$licznik = $licznik + 1;
+		if ($licznik%2 == 0)
+		{
+			echo '<td>'.$liczba.$litera."</td>";
+		}
+		else
+		{
+			echo '<td bgcolor="#FF0000">'.$liczba.$litera."</td>";
+		}
 	}
 	echo "<tr/>";
 }
