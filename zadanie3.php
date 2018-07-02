7ae613d0a16f624b076f8069f36253d203dad254
Pawel Lica <pawel.lica@gmail.com>
2018-07-02 15:44:11 +0200

    To przez stres..

--- zadanie3.php
+++ zadanie3.php
@@ -1,3 +1,3 @@
 <?php
-$camp = [1=>'Paweł','1'=>'Lica', 1.00=>'Bydgoszcz', true=>'Gniewomir'];
+$camp = [1=>'Paweł','1'=>'Lica', 1.00=>'Bydgoszcz', true=>'Gniewko'];
 var_dump($camp);
--- zadanie4.php
+++ zadanie4.php
@@ -1,5 +1,5 @@
 <?php
-$camp = [1=>'Paweł','1'=>'Lica', 1.00=>'Bydgoszcz', true=>'Gniewomir'];
+$camp = [1=>'Paweł','1'=>'Lica', 1.00=>'Bydgoszcz', true=>'Gniewko'];
 var_dump($camp);
 echo '<br>';
 $camp[4]=null; 
