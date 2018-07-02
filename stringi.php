e3a098525407d7d0743d5b2a9c9a058d8717f5a3
agata <agata.a.major@gmail.com>
2018-07-02 22:00:53 +0200

    stringi

---
+++ stringi.php
@@ -0,0 +1,28 @@
+<?php
+echo "Uczesticzę w \"PHP CAMP\" #2";
+echo "<br />\n";
+echo "Na $BANK się czegoś nauczę!";
+echo "<br />\n";
+echo "Jak to usunąłeś C:\\moje_filmy?";
+echo "<br />\n";
+echo "Szklanka jest w 1/n pełna";
+echo "<br />\n";
+echo "Dla n = 2 szklanka jest w ½ pełna";
+echo "<br />\n";
+echo "I'm a little mama'a boy";
+echo "<br />\n";
+
+
+echo 'Uczesticzę w \"PHP CAMP\" #2';
+echo "<br />\n";
+echo 'Na $BANK się czegoś nauczę!';
+echo "<br />\n";
+echo 'Jak to usunąłeś C:\\moje_filmy?';
+echo "<br />\n";
+echo 'Szklanka jest w 1/n pełna';
+echo "<br />\n";
+echo 'Dla n=2 szklanka jest w ½ pełna';
+echo "<br />\n";
+echo 'I\'m a little mama\'s boy';
+echo "<br />\n";
+?>
