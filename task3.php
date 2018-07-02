f3771035f0a11940d023a601571dc551502ff4c6
Klara <kucinska.klara@gmail.com>
2018-07-02 15:52:02 +0200

    poprawka

--- task3.php
+++ task3.php
@@ -1,3 +1,14 @@
 <?php
-$camp=[1 =>'Klara', '1'=>'Kucińska', 1.99 => 'Warszawa', true => 'Gniewko'];
+$camp=[1 =>'Klara', '1'=>'Kucińska', 1.99 => 'Warszawa', true => 'Gniewko', 4 => 'cos innego'];
+var_dump($camp);
+echo '<br>';
+array_push($camp, 'bardziej nowy element');
+var_dump($camp);
+echo '<br>';
+unset($camp[5]);
+array_push($camp, "Ala też ma zawsze 'jakiś ale");
+var_dump($camp);
+echo '<br>';
+$tab=[1,2,3,4,5];
+array_push($camp, $tab);
 var_dump($camp);
