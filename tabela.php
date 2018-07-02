b1d52f94c99659068ea7c3d615537a74e0234ece
Michal Obielak <mobielak7@gmial.com>
2018-07-02 16:27:09 +0200

    tabela vol.1

---
+++ tabela.php
@@ -0,0 +1,29 @@
+﻿<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset=utf-8" />
+		<title>Zadanie</title>
+	</head>
+
+	<body>
+		<table>
+        	<?php
+				$array;
+				$z;
+				for($i=0;$i<20;$i++){
+					for($j=0;$j<20;$j++){
+						$z=65+$j;
+						
+						$array[$i][$j]=$i.chr($z);}}
+						
+				for($a=0;$a<20;$a++){
+					echo "<tr>";
+					for($b=0;$b<20;$b++){
+						echo "<td>";
+						echo $array[$a][$b];
+						echo "</td>";}
+						echo "</tr>";}
+			?>
+        </table>
+    </body>
+</html>
