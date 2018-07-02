48c5e9fc8a5806de47fe1ed8be4bace2814d7aea
jPrzeworski <jakub.przeworski@onet.com.pl>
2018-07-02 14:15:53 +0200

    Add plik.php

---
+++ plik.php
@@ -0,0 +1,18 @@
+﻿<?php
+$a = "Uczestniczę w 'PHP CAMP' #2
+Na \$BANK się czegoś nauczę!
+Jak to usunąłeś C:\\\moje_filmy?
+Szklanka jest w 1\\n pełna
+Dla n = 2 szklanka jest w ½ pełna
+I'm a little mama's boy";
+
+$b = 'Uczestniczę w "PHP CAMP" #2
+Na $BANK się czegoś nauczę!
+Jak to usunąłeś C:\\\moje_filmy?
+Szklanka jest w 1\\n pełna
+Dla n = 2 szklanka jest w ½ pełna
+I\'m a little mama\'s boy';
+
+echo $a;
+echo "<br><br>";
+echo $b;
