6f306158bb745ec48e6b551b6c1c3bd6843f55dd
Gringeril <gabisska@gmail.com>
2018-07-02 15:40:52 +0200

    Task3.2

--- tablice.php
+++ tablice.php
@@ -7,6 +7,15 @@ $camp['1'] = "Woronowska";
 $camp[1.99] = "Szczecin";
 $camp[true] = "Gniewko";
 
-echo var_dump($camp)
+$camp[4] = "Cztery";
+$camp[5] = 'Kot ma Alę';
+$camp[6] = "Ala też zawsze ma 'jakieś ale'";
+
+for($i=1;$i<=5;$i++)
+{
+  $camp[] = $i;
+}
+
+echo var_dump($camp);
 
 ?>
