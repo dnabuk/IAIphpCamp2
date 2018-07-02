b3c0da3dd948aa93efb3c691c1a347b17b27f1e7
Jakub Dziedzic <dnabuk@gmail.com>
2018-07-02 15:47:44 +0200

    tablica v2

--- 3.php
+++ 3.php
@@ -9,4 +9,19 @@ $tablica = array (
 
 var_dump($tablica);
 
+$tablica[4] = "";
+var_dump($tablica);
+
+$tablica[] = "Kot ma ale";
+var_dump($tablica);
+
+unset($tablica[4]);
+var_dump($tablica);
+
+$tablica[] = "Ala tez zawsze ma zawsze jakies ale";
+var_dump($tablica);
+
+$tablica[] = array(range(1,5));
+var_dump($tablica);
+
 ?>
