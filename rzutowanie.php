fb7e9ab9ee6a637222097a1b597d07801b8fccf5
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-02 23:29:28 +0200

    Sprawdzenie wartości i typów w rzutowanie.php

--- rzutowanie.php
+++ rzutowanie.php
@@ -1,3 +1,14 @@
 <?php
+	echo "1+'1'" . " - wynik: " . strval(1+'1') . " - typ: " . gettype(1+'1') . "<br>";
+	echo "1+'-1.1'" . " - wynik: " . strval(1+'-1.1') . " - typ: " . gettype(1+'-1.1') . "<br>";
+	echo "1+'-0.3e3'" . " - wynik: " . strval(1+'-0.3e3') . " - typ: " . gettype(1+'-0.3e3') . "<br>";
+	echo "1 + '30 uczestników'" . " - wynik: " . strval(1 + '30 uczestników') . " - typ: " . gettype(1 + '30 uczestników') . "<br>";
+	echo "1 + '#2 PHP Camp'" . " - wynik: " . strval(1 + '#2 PHP Camp') . " - typ: " . gettype(1 + '#2 PHP Camp') . "<br>";
+	echo "'1' + 1" . " - wynik: " . strval('1' + 1) . " - typ: " . gettype('1' + 1) . "<br>";
+	echo "1 . 1 + 1" . " - wynik: " . strval(1 . 1 + 1) . " - typ: " . gettype(1 . 1 + 1) . "<br>";
+	echo "1 + 1 . 1" . " - wynik: " . strval(1 + 1 . 1) . " - typ: " . gettype(1 + 1 . 1) . "<br>";
+	echo "1. . 1 + 1" . " - wynik: " . strval(1. . 1 + 1) . " - typ: " . gettype(1. . 1 + 1) . "<br>";
 	
 ?>
+
+
