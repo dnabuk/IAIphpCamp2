7156fcbd2bec62795c8643cd76688422d899d51d
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-07-02 18:11:21 +0200

    to late

--- TabZad.php
+++ TabZad.php
@@ -4,7 +4,13 @@ $lettersTable = range('A', 'T');
 foreach ($numbersTable as $number){
     echo "<div>";
     foreach ($lettersTable as $letters) {
+        if ($number % 2 == 0) {
+            echo '<div style="display: inline-block; background-color: #000080; width: 50px; height: 50px; border: solid 1px black;">';
+        } else {
+            echo '<div style="display: inline-block; background-color: #800000; width: 50px; height: 50px; border: solid 1px black;">';
+        }
         echo $number.$letters;
+        echo "</div>";
     };
     echo "</div>";
 }
