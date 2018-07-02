64567766dcbd8a4ee73bb134a302426ff35de9bc
Michal Obielak <mobielak7@gmial.com>
2018-07-02 16:36:15 +0200

    tabela vol.2

--- tabela.php
+++ tabela.php
@@ -3,13 +3,21 @@
 	<head>
 		<meta charset=utf-8" />
 		<title>Zadanie</title>
+        <style>
+        td{
+        border:1px solid #000;
+        margin:0px;}
+        </style>
+        
 	</head>
 
 	<body>
 		<table>
         	<?php
+				
 				$array;
 				$z;
+				$licznik=0;
 				for($i=0;$i<20;$i++){
 					for($j=0;$j<20;$j++){
 						$z=65+$j;
@@ -17,9 +25,12 @@
 						$array[$i][$j]=$i.chr($z);}}
 						
 				for($a=0;$a<20;$a++){
+					$licznik++;
 					echo "<tr>";
 					for($b=0;$b<20;$b++){
-						echo "<td>";
+						$licznik++;
+						if($licznik%2){
+						echo "<td bgcolor=#CC6600>";}else{echo "<td>";};
 						echo $array[$a][$b];
 						echo "</td>";}
 						echo "</tr>";}
