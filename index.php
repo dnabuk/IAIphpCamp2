87bd3a4e17ffa0a3ff3dd2bc72af4e97d18b0854
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-02 14:44:22 +0200

    finished

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ if(isset($_GET['hint'])){
     }
 }
 
-
+echo '2'.='5';
 echo <<<'TAG'
 Uczestniczę w "PHP CAMP" #2
 TAG
@@ -35,7 +35,7 @@ echo 'I\'m a little mama\'s boy'.'<br>';
 echo '<br>';
 echo "Uczestniczę w \"PHP CAMP\" #2"."<br>";
 echo "Na \$BANK się czegoś nauczę!"."<br>";
-echo "Jak to usunąłeś C:\\\moje_filmy?"."<br>";
+echo "Jak to usunąłeś C:\\\\noje_filmy?"."<br>";
 echo "Szklanka jest w 1\\n pełna"."<br>";
 echo "Dla n = 2 szklanka jest w \u{00bd} pełna"."<br>";
 echo "I'm a little mama's boy"."<br>";
