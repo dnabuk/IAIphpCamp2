8550dafe5f71c4fa68d6b11cadc84f12310d95d1
Dagmara <dagaxdd@hotmail.com>
2018-07-03 01:47:56 +0200

    szachownica

--- tablica.php
+++ tablica.php
@@ -1,10 +1,22 @@
 <?php
-
+$numerCelki = 0;
 echo "<table cellspacing='0px'; border='2px'; cellpadding='15px' style='border-collapse: collapse'>";
 foreach (range(0, 19) as $liczba){
+    $liczbaParzysta = $liczba % 2 == 0;
 echo "<tr>";
     foreach (range("A", "T") as $litera){
-    echo "<row><td>$liczba $litera</td></row>";
+        $numerCelki = $numerCelki + 1;
+        $jasnaCellka = $numerCelki % 2 == 0;
+        if($liczbaParzysta){
+            $jasnaCellka = !$jasnaCellka;
+        }
+        if($jasnaCellka){
+            $kolor = "#dddddd";
+        } else {
+            $kolor = "#555555";
+        }
+
+        echo "<td style='background: $kolor'>$liczba $litera</td>";
     }
     echo "</tr>";
 }
