06a87af54ed15f9cd49e034040ded6bf54590720
Klara <kucinska.klara@gmail.com>
2018-07-02 14:27:01 +0200

    no message

--- task1.php
+++ task1.php
@@ -1,2 +1,2 @@
 <?php
-echo "Uczestniczę w \"PHP CAMP\" #2 Na \$BANK \n się czegoś nauczę! \n Jak to usunąłeś C:\\\moje_filmy? \n Szklanka jest w 1/n pełna \n Dla n=2 szkalnka jest w 1/2 pełna \n I'm a little mama's boy";
+echo "Uczestniczę w \"PHP CAMP\" #2 <br> Na \$BANK się czegoś nauczę! <br> Jak to usunąłeś C:\\\moje_filmy? <br> Szklanka jest w 1/n pełna <br> Dla n=2 szkalnka jest w 1/2 pełna <br> I'm a little mama's boy";
