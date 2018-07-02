0f2e152fc5fc8b1b6bba6d17d06c895f68bfdb02
Pawel Lica <pawel.lica@gmail.com>
2018-07-02 16:27:27 +0200

    Po przekatnej

--- zadanie5.php
+++ zadanie5.php
@@ -3,17 +3,20 @@ $tab1=range(0, 19, 1);
 $tab2=range('A', 'T', 1);
 $dziwnaTablica = [$tab1, $tab2];
 echo '<table>';
-$i=0;
+
+$j=0;
 foreach ($dziwnaTablica[0] as $valX){
 	echo '<tr>';
+	$i=0;
 	foreach ($dziwnaTablica[1] as $valY){
-		if ($i%2==0){
+		
+		if ($i>$j){
 			echo '<td bgcolor=brown>'.$valX.$valY.'</td>';
 		} else {
 			echo '<td bgcolor=yellow>'.$valX.$valY.'</td>';
 		}
 		$i++;
 	}
-	$i++;
+	$j++;
 	echo '</tr>';
 }
