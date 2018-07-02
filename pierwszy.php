0e6a45c768c797a468227edf3b781f1bcebf43af
Marta Jaworska <maartajaworska@gmail.com>
2018-07-02 14:59:01 +0200

    pierwszy program

---
+++ pierwszy.php
@@ -0,0 +1,18 @@
+<html>
+<body>
+<?php 
+echo "Uczestniczę w \"PHP CAMP\" #2 <br/>";
+echo 'Uczestniczę w "PHP CAMP" #2 <br/>';
+echo "Na \$BANK się czegoś nauczę!<br/>";
+echo 'Na $BANK się czegoś nauczę!<br/>';
+echo "Jak to usunąłeś C:\\\\moje_filmy? <br/>";
+echo 'Jak to usunąłeś C:\\\\moje_filmy?<br/>';
+echo "Szklanka jest w 1\\n pełna <br/>";
+echo 'Szklanka jest w 1\n pełna <br/>';
+echo "Dla n = 2 szklanka jest w ½ pełna<br/>";
+echo 'Dla n = 2 szklanka jest w ½ pełna<br/>';
+echo "I'm a little mama's boy<br/>";
+echo 'I\'m a little mama\'s boy<br/>';
+?>
+</body>
+</html>
