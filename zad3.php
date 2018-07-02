effca3b4e84c28ef0fa9fe99c53e3c1e63837e5a
Adrian Pabisiak <adrianpabisiak2@gmail.com>
2018-07-02 15:52:10 +0200

    zad3

--- zad3.php
+++ zad3.php
@@ -7,10 +7,13 @@ $b=4;
 $camp = array($a=>"Adrian",
 	 "1"=>"Pabisiak",
 	 "1.99"=>"Szczecin",
-	 true=>"Gwniewko");
-	 echo $camp;
+	 true=>"Gwniewko"
+	 );
+	// echo $camp;
+	 $camp[$b];
+	 $camp[5]="kot ma Alê";
+	 unset($camp[5]);
 	 
-	 //unset($camp['$b'])
 
 
 ?>
