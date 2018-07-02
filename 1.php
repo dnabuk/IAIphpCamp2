8a200856a3e878feaf8e048084e4ae651667230e
Jakub Dziedzic <dnabuk@gmail.com>
2018-07-02 15:03:55 +0200

    nie dodalo sie wczesniej :o

---
+++ 1.php
@@ -0,0 +1,19 @@
+<?php
+echo<<<END
+	Uczestniczę w "PHP CAMP" #2<br>
+	Na $\BANK się czegoś nauczę!<br>
+	Jak to usunąłeś C:\\\moje_filmy?<br>
+	Szklanka jest w 1\\n pełna<br>
+	Dla n=2 szkladka jest w &#189 pełna<br>
+	I\'m little mama\'s boy<br><br><br>
+END;
+
+echo<<<END
+ 	"Uczestniczę w \"PHP CAMP\" #2<br>
+	Na \$BANK się czegoś nauczę!<br>
+	Jak to usunąłeś C:\\\moje_filmy?<br>
+	Szklanka jest w 1\\n pełna<br>
+	Dla n=2 szkladka jest w &#189 pełna<br>
+	I'm little mama's boy
+END;
+?>
