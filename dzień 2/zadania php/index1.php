8491e0a72dfb6e2ccb61328dcc925dbf17ebd5a7
Piotr <pittero1@wp.pl>
2018-07-03 12:34:17 +0200

    dzien 2 - bazy danych v5

--- dzień 2/zadania php/index1.php
+++ dzień 2/zadania php/index1.php
@@ -20,22 +20,19 @@ $los = rand(0, (count($panstwa)-1));
   Liczba zamówień:<br/>
   <input type="text" name="orders_count" value="<?php echo rand(1, 10000); ?>" disabled><br/>
   Ulica:<br/>
-  <input type="date" name="street"><br/>  
+  <input type="text" name="street"><br/>  
   Miasto:<br/>
-  <input type="date" name="city"><br/>
+  <input type="text" name="city"><br/>
   Kod pocztowy:<br/>
-  <input type="date" name="postcode"><br/>   
+  <input type="text" name="postcode"><br/>   
   Państwo:<br/>
   <input type="text" name="country" value="<?php echo $panstwa[$los]; ?>"><br/>  
   Notatki:<br/>
-  <textarea rows="4" cols="50">Przykładowa notatka klienta :)</textarea>  
-  <br/><input type="submit" value="Zapisz klienta!">
-</form>
+  <textarea rows="4" cols="50" name="notes">Przykładowa notatka klienta :)</textarea>  
+  <br/><input type="submit" value="Zapisz klienta!" name="zapisz">
+</form><br/><br/>
 <h2>Przykładowe 100 rekordów najnowszych :)</h2>
 <?php 
-
-print_r($_POST);
-
 $servername = 'localhost';
 $username = 'root';
 $password = '';
@@ -45,7 +42,40 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
+function protect($string)
+{
+	return htmlspecialchars($string);
+}
+#Zapisywanie klienta start
+if(isset($_POST['zapisz']))
+{
+	var_dump($_POST);
+	
+	$name = protect($_POST['name']);
+	$surname = protect($_POST['surname']);
+	$date_of_birth = protect($_POST['date_of_birth']);
+	$orders_count = protect($_POST['orders_count']);
+	$street = protect($_POST['street']);
+	$city = protect($_POST['city']);
+	$postcode = protect($_POST['postcode']);	
+	$country = protect($_POST['country']);	
+	$notes = protect($_POST['notes']);
+	
+	
+	$sql = 'INSERT INTO clients (name, surname, gender, date_of_birth, orders_count, street, city, postcode, country, notes)
+	VALUES (`'.$name.'`, `'.$surname.'`, `'.$date_of_birth.'`, `'.$orders_count.'`, `'.$street.'`, `'.$city.'`, `'.$postcode.'`, `'.$country.'`, `'.$notes.'`)';
 
+	if ($conn->query($sql) === TRUE)
+	{
+		echo '<h1><font color="red">Dodano klienta!</font></h1>';
+	}
+	else
+	{
+		echo 'Error: ' . $sql . '<br/>' . $conn->error;
+	}
+}
+#Zapisywanie klienta end
+#Wyświetlanie klienta start
 $sql = "SELECT * FROM clients ORDER BY id DESC LIMIT 100";
 $result = $conn->query($sql);
 
@@ -56,5 +86,6 @@ if ($result->num_rows > 0) {
 } else {
     echo "0 results";
 }
+#Wyświetlanie klienta end
 $conn->close();
 ?>
