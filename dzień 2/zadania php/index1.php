c649d55bef6d67e4e5eb12a178aa9dd68b106755
Piotr <pittero1@wp.pl>
2018-07-03 11:49:51 +0200

    dzien 2 - bazy danych v2

--- dzień 2/zadania php/index1.php
+++ dzień 2/zadania php/index1.php
@@ -9,12 +9,12 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "SELECT * FROM clients LIMIT 100";
+$sql = "SELECT * FROM clients ORDER BY id DESC LIMIT 100";
 $result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        echo 'id: ' . $row['id'];
+        echo 'id: ' . $row['id']. 'imie: '.$row['surname'].'<br/>';
     }
 } else {
     echo "0 results";
