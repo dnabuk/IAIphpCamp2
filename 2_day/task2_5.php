b6f58ea86574c452fc2d349021cc0bc14b0cf5fb
Krzysztof Margański <bystek@o2.pl>
2018-07-03 16:08:04 +0100

    day 2 po awari zabezpieczanie formularza2

---
+++ 2_day/task2_5.php
@@ -0,0 +1,41 @@
+<form method="POST">
+	Name:<input type="text" name="name">
+	Surname:<input type="text" name="surname">
+	Gender:<select name='gender'>
+		<option value="male">Male</option>
+		<option value="female">Female</option>
+		<option value="unknow">Unknow</option>
+	</select>
+	Birth date:<input type="date" name="birth_date">
+	Street:<input type="text" name="street">
+	City:<input type="text" name="city">
+	Postcode:<input type="text" name="post_code">
+	<input type="submit" name='submit' value="Wyślij">
+</form>
+
+
+<?php 
+require 'connect.php';
+
+if(isset($_POST['name']))
+{
+	$name=$_POST['name'];
+$surname=$_POST['surname'];
+$gender=$_POST['gender'];
+$birth_date=$_POST['birth_date'];
+$street=$_POST['street'];
+$city=$_POST['city'];
+$post_code=$_POST['post_code'];
+	
+	$query_ins="INSERT INTO `clients` values(null,'$name','$surname','$gender','$birth_date',12,'$street','$city','$post_code','1','')";
+		
+if(!$result= mysqli_query($link,$query_ins)){
+		echo (mysqli_error($link));}else{
+	echo 'wysłano';
+}
+}
+//$query="SELECT name, surname, city FROM `clients` limit 10";
+//while ($data= mysqli_fetch_assoc($result)) 
+//	var_dump($data);
+//}
+?>
--- 2_day/task3.php
+++ 2_day/task3.php
@@ -1,9 +1,10 @@
 <?php 
 require 'connect.php';
 $id=$_GET['id'];
-$query='SELECT name, surname, city FROM `clients` where id=' . mysqli_real_escape_string($link,$id);
+$query="SELECT name, surname, city FROM `clients` where id=$id"; //. mysqli_real_escape_string($link,$id);
 if($result= mysqli_query($link,$query)){
 while ($data= mysqli_fetch_assoc($result)) {
-	var_dump($data);
+	//var_dump($data);
+	echo $data['name'];
 }
 }
