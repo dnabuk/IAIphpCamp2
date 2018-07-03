267621e2291f9ac85360dbafc4c157a30c2c208d
Pawel Lica <pawel.lica@gmail.com>
2018-07-03 18:44:00 +0200

    Dodanie przycisku do edycji

---
+++ wyswietlanie z edycja index.php
@@ -0,0 +1,79 @@
+<?php
+$link = mysqli_connect("localhost", "php", "dlTPxr4tHUmefOMR");
+
+mysqli_select_db($link, "phpcamp_plica");
+
+$query = "SELECT count(*) AS 'max' FROM `clients` WHERE 1";
+$resultHandle = mysqli_query($link, $query);
+$row = mysqli_fetch_assoc($resultHandle);
+
+$naStrone = 100;
+$max = round(($row['max']/$naStrone), 0, PHP_ROUND_HALF_DOWN);
+
+//var_dump($max);
+
+$strona=0;
+if (!empty($_GET['s'])) {
+	$strona = $_GET['s'];
+}
+if ($strona>=$max) $strona=$max;
+if ($strona<0) $strona=0;
+if ($strona > 0) {
+	$poprzednia=$strona-1;
+	echo '<a href=?s='.$poprzednia.'><--poprzednia</a> '.$strona.' ';
+}
+if ($strona < $max) {
+	$nastepna=$strona+1;
+	echo '<a href=?s='.$nastepna.'>nastepna --></a>';
+}
+
+if (!empty($_GET['edit'])) {
+	$eID = $_GET['edit'];
+}
+
+$query = "SELECT * FROM `clients` WHERE 1 limit $naStrone OFFSET ".($strona*$naStrone);
+$resultHandle = mysqli_query($link, $query);
+
+if (mysqli_num_rows($resultHandle) > 0) {
+	echo '<table>';
+	$i=0;
+	echo '<tr><td>nr</td><td>ID</td><td>Imię</td><td>Nazwisko</td><td>Płeć</td>';
+	echo '<td>Data urodzenia</td><td>Ulica</td><td>Miasto</td><td>Kod pocztowy</td><td>Edycja</td>';
+    while($row = mysqli_fetch_assoc($resultHandle)) {
+        //var_dump($row);
+		
+		echo '<tr><td>'.($strona*$naStrone+$i).'</td>';
+		echo '<td>'.$row['id'].'</td>';
+		if ($eId===$row['id']){
+		
+		}
+		echo '<td>'.$row['name'].'</td>';
+		echo '<td>'.$row['surname'].'</td>';
+		echo '<td>'.$row['gender'].'</td>';
+		echo '<td>'.preg_replace('/\s[0-9]{2}:[0-9]{2}:[0-9]{2}/', '', $row['date_of_birth']).'</td>';
+		echo '<td>'.$row['street'].'</td>';
+		echo '<td>'.$row['city'].'</td>';
+		echo '<td>'.$row['postcode'].'</td>';
+		echo '<td><a href="?s='.$strona.'&edit='.$row['id'].'">edytuj</a></td>';
+		
+		/*
+		foreach ($row as $col){
+			echo '<td>'.preg_replace('/\s[0-9]{2}:[0-9]{2}:[0-9]{2}/', '', $col).'</td>'; //tak, poszedłem na łatwiznę ;)
+		}
+		*/
+		echo '</tr>';
+		$i++;
+    }
+	echo '</table>';
+} else {
+	echo 'Brak wyników';
+}
+
+if ($strona > 0) {
+	$poprzednia=$strona-1;
+	echo '<a href=?s='.$poprzednia.'><--poprzednia</a> '.$strona.' ';
+}
+if ($strona < $max) {
+	$nastepna=$strona+1;
+	echo '<a href=?s='.$nastepna.'>nastepna --></a>';
+}
