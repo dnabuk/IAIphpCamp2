12212ef7f3bb51baffbe4aada4fb005c661e3343
Pawel Lica <pawel.lica@gmail.com>
2018-07-03 20:20:47 +0200

    Wydaje się działać

--- wyswietlanie z edycja index.php
+++ wyswietlanie z edycja index.php
@@ -34,8 +34,20 @@ if (!empty($_GET['edit'])) {
 }
 
 if (!empty($_POST)){
-	$eID=null;
+	$eID=$_POST['eID'];
+	$name = $_POST['name'];
+	$surname = $_POST['surname'];
+	$street = $_POST['street'];
+	$city = $_POST['city'];
+	$code = $_POST['code'];
 	
+	$query = "UPDATE `clients` SET `name` = '$name', `surname` = '$surname', `street` = '$street', `city` = '$city', `postcode` = '$code' WHERE `clients`.`id` = $eID";
+	//echo '<br>'.$query.'<br>';
+	
+	if (!mysqli_query($link, $query)) {
+		echo "<br />BŁĄD AKTUALIZACJI: " . mysqli_error($link).'<br />';
+	}
+	$eID=null;
 }
 
 $query = "SELECT * FROM `clients` WHERE 1 limit $naStrone OFFSET ".($strona*$naStrone);
@@ -63,12 +75,13 @@ if (mysqli_num_rows($resultHandle) > 0) {
 		} else {
 			echo '<form method="POST" action="index.php?s='.$strona.'">';
 			echo '<td><input type="text" name="name" value="'.$row['name'].'" /></td>';
-			echo '<td><input type="text" name="surnamename" value="'.$row['surname'].'" /></td>';
+			echo '<td><input type="text" name="surname" value="'.$row['surname'].'" /></td>';
 			echo '<td>'.$row['gender'].'</td>'; //select.. tylko jak łatwo zrobić wartość domyślną i pozostałe, żeby się nie powtarzały.. No nic, nie ma zmiany płci.
-			echo '<td><input type="date" name="date_of_birth" value="'.preg_replace('/\s[0-9]{2}:[0-9]{2}:[0-9]{2}/', '', $row['date_of_birth']).'" /></td>';
+			echo '<td>'.preg_replace('/\s[0-9]{2}:[0-9]{2}:[0-9]{2}/', '', $row['date_of_birth']).'</td>';
 			echo '<td><input type="text" name="street" value="'.$row['street'].'" /></td>';
 			echo '<td><input type="text" name="city" value="'.$row['city'].'" /></td>';
-			echo '<td><input type="text" name="name" value="'.$row['postcode'].'" /></td>';
+			echo '<td><input type="text" name="code" value="'.$row['postcode'].'" /></td>';
+			echo '<input type="hidden" name="eID" value="'.$eID.'" />';
 			echo '<td><input type="submit" /></td></form>'; //zapis metodą post, jakiś token?
 		}
 		/*
