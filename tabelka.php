20611af07b89416b7e3f106b013e0bb37cdba1e3
Michal Obielak <mobielak7@gmial.com>
2018-07-03 14:51:12 +0200

    bez zabezpieczenia

---
+++ tabelka.php
@@ -0,0 +1,9 @@
+﻿<?php
+	include("connect.php");
+
+	$i=$_GET['id'];
+	$zap="select * from clients where id=$i";
+	$wynik=mysqli_query($link,$zap);
+	while($w=mysqli_fetch_row($wynik)){
+		echo $w[1]." ".$w[2];}
+?>
