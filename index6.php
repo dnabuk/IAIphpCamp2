a61c8395e30424406fa2c75c372e37deb0d39e5a
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-03 14:13:59 +0200

    Dodanie mysqli_real_escape_string do geta

--- index6.php
+++ index6.php
@@ -2,29 +2,30 @@
 include('config.php');
 
 
+$cc = mysqli_connect('localhost','root','','phpcamp_hrzadzinski'); // zrobione po to aby móc użyć mysqlirealescapestring
 
+$get = mysqli_real_escape_string($cc,$_GET['page']);
 $pdo = GetPDO();
-if(isset($_GET['page'])){
-    if($_GET['page'] == 1){
+if(isset($get)){
+    if($get == 1){
 
         $lim = 100;
         $off = 0;
-    }else if($_GET['page'] == 2){
+    }else if($get == 2){
 
         $lim = 100;
         $off = 100;
     }
 }
 $sql = 'SELECT * FROM `clients` LIMIT '.$lim.' OFFSET '.$off.'';
+
 $c = GetPDO()->prepare($sql);
-//$c->bindParam(1,$lim);
-//$c->bindParam(2,$off);
 
 $c->execute();
 
 $result = $c->fetchAll();
 
-
+$cc->close(); //zamykanie połączenia z nieużywanym mysqli
 foreach($result as $row){
 
     echo '<a href="index7.php?id='.$row['id'].'">'.$row['name'].' '.$row['surname'].'</a><br>';
