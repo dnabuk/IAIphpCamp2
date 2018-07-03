0a1b8e58069551b3697482c4afb661b5f6d9217e
Krzysztof Margański <bystek@o2.pl>
2018-07-03 17:17:52 +0100

    sql join

--- 2_day/task.php
+++ 2_day/task.php
@@ -1,6 +1,6 @@
 <?php 
 require 'connect.php';
-$query="SELECT name, surname, city FROM `clients` limit 10";
+$query='SELECT * from `clients` INNER JOIN `orders` ON clients.id=orders.client_id where id=$id';
 $result= mysqli_query($link,$query);
 while ($data= mysqli_fetch_assoc($result)) {
 	var_dump($data);
--- 2_day/task3.php
+++ 2_day/task3.php
@@ -1,7 +1,7 @@
 <?php 
 require 'connect.php';
 $id=$_GET['id'];
-$query="SELECT name, surname, city FROM `clients` where id=$id"; //. mysqli_real_escape_string($link,$id);
+$query="SELECT * from `clients` INNER JOIN `orders` ON clients.id=orders.client_id where id=$id"; //. mysqli_real_escape_string($link,$id);
 if($result= mysqli_query($link,$query)){
 while ($data= mysqli_fetch_assoc($result)) {
 	//var_dump($data);
