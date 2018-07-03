6d5657c4c569a1c0052de1bc398eb71987dc2584
agata <agata.a.major@gmail.com>
2018-07-03 07:49:43 +0200

    rzuotowanie.kolory

--- rzutowanie.php
+++ rzutowanie.php
@@ -2,14 +2,24 @@
 $numberArray = range(0,19);
 $lettersArray = range('a','t');
 echo '<table border="1">';
+$i = 0;
 foreach ($numberArray as $numberKey => $numberValue ){
 echo '<tr>';
 
     foreach ($lettersArray as $letterKey => $letterValue){
-        echo '<td>';
+        $i++;
+        echo '<td ';
+        if ($i%2==0){
+            echo 'bgcolor="red"';
+        }
+        else {
+            echo 'bgcolor="yellow"';
+        }
+        echo '>';
         echo $numberValue . $letterValue ;
         echo '</td>';
     }
+    $i++;
     echo '</tr>';
 }
 echo '</table>';
