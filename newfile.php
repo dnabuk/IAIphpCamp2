2118d249e9a85f09d50493139d5368e3ebb46b62
kamilossan <vision@naszepolice.pl>
2018-07-03 06:53:35 +0200

    fixed order, changex axis

--- newfile.php
+++ newfile.php
@@ -1,17 +1,17 @@
 <?php
 $tablica = range("A","T");
 $tab2 = range(0, 19);
-foreach($tablica as $key=>$value){
-    $tablica[$key]=$tab2;
-    foreach($tab2 as $sub =>$subval){
-        $tablica[$key][$sub]= $key.$subval;
+foreach($tab2 as $key=>$value){
+    $tab2[$key]=$tablica;
+    foreach($tablica as $sub =>$subval){
+        $tab2[$key][$sub]= $value.$subval;
     }
 }
 echo '<body>';
 foreach($tablica as $key=>$value){
     echo '<br>';
     foreach($tab2 as $sub =>$subval){
-        echo '<div style="display: inline-block; background-color:white; width: 50px; height: 50px; border: solid 1px black;">'.$tablica[$key][$sub].'</div>';
+        echo '<div style="display: inline-block; background-color:white; width: 50px; height: 50px; border: solid 1px black;">'.$tab2[$key][$sub].'</div>';
     }
 }
 echo '</body>';
