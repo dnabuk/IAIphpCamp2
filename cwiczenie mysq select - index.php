6df42eda4058726bdbceec7c72f61182e88d2d43
Pawel Lica <pawel.lica@gmail.com>
2018-07-03 11:53:36 +0200

    Ćwiczenie 1. php + mysql, wybieranie kilku rekordów

---
+++ cwiczenie mysq select - index.php
@@ -0,0 +1,23 @@
+<?php
+
+$link = mysqli_connect("localhost", "root", "");
+mysqli_select_db($link, "phpcamp_plica");
+
+$query = "SELECT * FROM `clients` WHERE 1 limit 15";
+$resultHandle = mysqli_query($link, $query);
+
+if (mysqli_num_rows($resultHandle) > 0) {
+	echo '<table>';
+    while($row = mysqli_fetch_assoc($resultHandle)) {
+        //var_dump($row);
+		echo '<tr>';
+		foreach ($row as $col){
+			echo '<td>'.$col.'</td>';
+		}
+		echo '</tr>';
+		
+    }
+	echo '</table>';
+} else {
+	echo 'Brak wyników';
+}
