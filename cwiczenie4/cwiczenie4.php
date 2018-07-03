16e2475973e555f67e033d60da8b7a475379c698
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2018-07-03 16:21:52 +0200

    Changed totally view of solution.

--- cwiczenie4/cwiczenie4.php
+++ cwiczenie4/cwiczenie4.php
@@ -1,44 +1,14 @@
 <?php
 
+$name=htmlspecialchars((string) $_POST['name']);
+$surname=htmlspecialchars((string) $_POST['surname']);
+$gender=htmlspecialchars((string) $_POST['gender']);
+$city=htmlspecialchars((string) $_POST['city']);
 
-$servername = "localhost";
-$username = "patrycjusz";
-$password = "1234";
-$dbname = "phpcamp_pnowaczyk";
-
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+echo "$name<br>";
+echo "$surname<br>";
+echo "$gender<br>";
+echo "$city<br>";
 
 
-
-$sql = "SELECT id, name, surname, city FROM clients WHERE orders_count<'50'";
-
-
-
-
-$result = mysqli_query($conn, $sql);
-
-var_dump($result);
-
-echo "</br></br>";
-
-
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["name"]. " " . $row["surname"] . " - City: " . $row["city"] . "<br>";
-    }
-} else {
-    echo "0 results";
-}
-
-
-
-
-mysqli_close($conn);
-?>
+ ?>
