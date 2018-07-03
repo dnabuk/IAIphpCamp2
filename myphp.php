2c8cc3a25ee5bdd5110b3d205a33d2de29e69f23
Daniel Molenda <danielmolenda16@gmail.com>
2018-07-03 12:17:46 +0200

    mysql php

---
+++ myphp.php
@@ -0,0 +1,10 @@
+<?php
+    $link = mysqli_connect ("127.0.0.1", "root", "","phpcamp_dmolenda");
+    mysqli_select_db($link, "phpcamp_dmolenda");
+    $sql = "SELECT * FROM `clients` LIMIT 10";
+    $result2 = $link->query($sql);
+
+    foreach($result2 as $row2){
+        echo $row2['name'].' '.$row2['surname'].'<br>';
+    }
+?>
