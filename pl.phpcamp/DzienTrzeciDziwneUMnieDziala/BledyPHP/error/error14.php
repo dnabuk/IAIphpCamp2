9bfbc2f42507925fa023b589911721925515f2c8
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-07-04 16:22:56 +0200

    error 14 if A, G

--- pl.phpcamp/DzienTrzeciDziwneUMnieDziala/BledyPHP/error/error14.php
+++ pl.phpcamp/DzienTrzeciDziwneUMnieDziala/BledyPHP/error/error14.php
@@ -6,43 +6,50 @@ class A {
     private $test = 'test';
     public function test()
     {
-        return date('H:m:s');
+        return date('H:i:s');
     }
 }
 class B extends A {
 
     public function test()
     {
-        return date('H:m:s');
+        return date('H:i:s');
     }
 }
 class E extends A {
 
     public function test()
     {
-        return date('H:m:s');
+        return date('H:i:s');
     }
 }
 class D extends A {
 
     public function test()
     {
-        return date('H:m:s');
+        return date('H:i:s');
     }
 }
 class C extends A {
 
     public function test()
     {
-        return date('H:m:s');
+        return date('H:i:s');
     }
 }
-class G {
+class G extends A {
 
     public function test2()
     {
-        return date('H:m:s');
+        return date('H:i:s');
     }
 }
 $name = $_GET['name'];
-var_Dump((new $name())->test());
+echo '<pre>';
+if (($name >= 'A') && ($name <= 'G') || ($name == 'G')){
+    var_Dump((new $name())->test());
+}else{
+    echo 'nic';
+}
+
+echo '</pre>';
