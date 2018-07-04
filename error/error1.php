c10b02fe1ed37ab0044998f65253a1c37be1a989
Pawel Lica <pawel.lica@gmail.com>
2018-07-04 14:13:52 +0200

    Poprawiono

--- error/error1.php
+++ error/error1.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ERROR | E_WARNING | E_PARSE);
+error_reporting(E_ALL);
 
 function foo()// usunięty argument($a) - nie był wykorzystywany
 {
--- error/error2.php
+++ error/error2.php
@@ -1,6 +1,6 @@
 <?php
 
-error_reporting(E_ALL);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 function foo($a)
 {
