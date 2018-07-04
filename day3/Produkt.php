4d67357f25e075576cda59cc8b44f5939710d136
Jakub Dziedzic <dnabuk@gmail.com>
2018-07-04 09:55:37 +0200

    utworzenie setterow

--- day3/Produkt.php
+++ day3/Produkt.php
@@ -1,20 +1,44 @@
 <?php
 
 class Produkt {
-	public $nazwa;
-	public $cena;
+	private $nazwa;
+	private $cena;
 	private $zdjecie;
 	private $kategoria;
 	private $kolor;
 	private $jakosc;
 
-	public function __construct(){
+	public function __construct() {
 		$this->nazwa = "Produkt";
 		$this->cena = "0";
 		$this->zdjecie = "zdjecie.jpg";
 		$this->kategoria = "kategoria";
 		$this->kolor = "kolor";
 		$this->jakosc = "jakosc";
+	}
+
+	public function setNazwa($nazwa) {
+		$this->nazwa = $nazwa;            
+	}
+
+	public function setCena($cena) {
+		$this->cena = $cena;            
+	}
+
+	public function setzdjecie($zdjecie) {
+		$this->zdjecie = $zdjecie;            
+	}
+
+	public function setkategoria($kategoria) {
+		$this->kategoria = $kategoria;            
+	}
+
+	public function setkolor($kolor) {
+		$this->kolor = $kolor;            
+	}
+
+	public function setjakosc($jakosc) {
+		$this->jakosc = $jakosc;            
 	}
 
 	public function __destruct(){
@@ -24,6 +48,6 @@ class Produkt {
 
 
 $produkt = new Produkt();
+$produkt->setNazwa("dasdas");
+var_dump($produkt);
 die('das');
-
-?>
