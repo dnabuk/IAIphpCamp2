15be3f6c550ef58eddc4a63351e5c455c19d0acb
Pawel Lica <pawel.lica@gmail.com>
2018-07-04 15:21:31 +0200

    Poprawiono return i porównanie w case 2

--- error/error9.php
+++ error/error9.php
@@ -8,6 +8,7 @@ class A {
         foreach ($basket as $product) {
             $amount += $product['quantity'] * $product['price'];
         }
+		return $amount;
     }
 }
 
@@ -32,20 +33,23 @@ if ($paymentAmount == $objA->getAmount($basket)) {
 
 echo '<h1>Case 2</h1>';
 $basket = [
-    ['productId' => 763, 'quantity' => 1, 'name' => 'Product 1' , 'price' => 1.2],
+    ['productId' => 763, 'quantity' => 1, 'name' => 'Product 1', 'price' => 1.2],
     ['productId' => 31238, 'quantity' => 2, 'name' => 'Product 2', 'price' => 3],
     ['productId' => 4523, 'quantity' => 1, 'name' => 'Product 3', 'price' => 5],
-    ['productId' => 19879, 'quantity' => 100, 'name' => 'Product 4',  'price' => 272.96],
+    ['productId' => 19879, 'quantity' => 100, 'name' => 'Product 4', 'price' => 272.96],
 ];
 $paymentAmount = 27308.2;
+
 var_dump('Koszyk', $basket);
 var_dump('******************************************************');
 var_dump('wysokość wpłaty: '.  $paymentAmount);
 var_dump('******************************************************');
 
-if ($paymentAmount == $objA->getAmount($basket)) {
+if (round($paymentAmount, 3) == round($objA->getAmount($basket), 3)) {
     echo 'wpłata pokrywa całą wartość koszyka';
 } else {
     echo 'Wartość wplaty jest inna niż wartość koszyka';
 }
 
+var_dump($objA->getAmount($basket));
+var_dump($paymentAmount);
