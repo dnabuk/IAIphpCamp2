bc2ae36d509a003a55c866673762caa6d7958e22
Pawel Lica <pawel.lica@gmail.com>
2018-07-04 15:49:30 +0200

    Ignorowanie wielkości liter

--- error/error12.php
+++ error/error12.php
@@ -7,7 +7,7 @@ class A {
     public function printInfo($name, $validateName)
     {
         if ($validateName == 1) {
-            if ( strpos($name, 'Mateusz') !== false ) {
+            if ( strpos(strtoupper($name), 'MATEUSZ') !== false ) { //nie będzie czuły na wielkość liter
                 //sprawdzamy czy imie mateusz znajduje sie w przekazanej nazwie
                 echo 'Witaj Mateusz!';
             } else {
