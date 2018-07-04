ed160b95a87e40475d6d0060b8640f121629579d
Gringeril <gabisska@gmail.com>
2018-07-04 14:24:15 +0200

    Errors #2

--- error/error3.php
+++ error/error3.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 
 if (!isset($value)) {
-    trigger_error('Error 3 !!!', E_USER_NOTICE );
+    trigger_error('Error 3 !!!', E_USER_WARNING );
 }
 
 var_Dump($value['a']['b']);
