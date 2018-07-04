391ba3ad2c1ae0f860b6bc90e87337c4bcac98d6
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-04 14:14:34 +0200

    Ustawienie komunikatu błędu w error3.php

--- error/error3.php
+++ error/error3.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 
 if (!isset($value)) {
-    trigger_error('Brak zmiennej $value');
+    trigger_error('Zmienna $value nie jest zdefiniowana');
 }
 
 var_Dump($value['a']['b']);
