6f7f185e96d437ae0a3a18ef6ddaba8c2b7c927c
Pawel Lica <pawel.lica@gmail.com>
2018-07-04 16:06:29 +0200

    zmienione m na i

--- error/error13.php
+++ error/error13.php
@@ -1,17 +1,20 @@
 <?php
-error_reporting(0);
+error_reporting(E_ALL);
 
 
 class A {
 
     public function getTime($timestamp)
     {
-        return date('H:m:s',$timestamp);
+        return date('H:i:s',$timestamp);
     }
 }
 
 
-echo 'Wynik dla timestamp = ' . 1464847575 . ' czwartek, 2 czerwca 2016 06:06:15';
-var_dump((new A)->getTime(1464847575));
-echo 'Wynik dla timestamp = ' . 1518614715 . ' środa, 14 lutego 2018 13:25:15';
-var_dump((new A)->getTime(1518614715));
+echo 'Wynik dla timestamp = ' . 1464847575 . ' czwartek, 2 czerwca 2016 06:06:15'; //2.06.2016, 08:06:15
+echo '<br>';
+var_dump((new A)->getTime(1464847575)); //08:06:15
+echo '<br>';
+echo 'Wynik dla timestamp = ' . 1518614715 . ' środa, 14 lutego 2018 13:25:15'; //14.02.2018, 14:25:15
+echo '<br>';
+var_dump((new A)->getTime(1518614715)); //14:02:15
