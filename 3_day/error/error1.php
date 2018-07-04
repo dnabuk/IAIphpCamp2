3a8bbc78a07415b64172933a20dd122d7da4a72c
Krzysztof Margański <bystek@o2.pl>
2018-07-04 14:16:40 +0100

    error1

---
+++ 3_day/error/error1.php
@@ -0,0 +1,12 @@
+<?php
+
+//error_reporting(0);
+
+function foo()
+{
+	echo 'foo';
+}
+foo();
+//var_dump($value['a']['b']);
+
+foo();
---
+++ 3_day/error/zadania_dodatkowe.php
@@ -0,0 +1,45 @@
+
+Zadanie 1.
+
+Święty Mikołaj dostarcza prezenty do nieskończonej, dwuwymiarowej siatki domów.
+Zaczyna od dostarczenia prezentu do domu w jego początkowej lokalizacji, a następnie elf z Bieguna Północnego dzwoni do niego przez radio i mówi mu, gdzie ma się dalej poruszać. Ruchy są zawsze dokładnie o jeden dom na północ (^), na południe (v), na wschód (>) lub na zachód (<). Po każdym przeprowadzeniu dostarcza kolejną prezentację do domu w swojej nowej lokalizacji.
+Jednak elf z powrotem na biegunie północnym ma trochę za dużo ajerkoniaki, więc jego wskazówki są trochę poza zasięgiem, a Mikołaj kończy odwiedzanie kilku domów więcej niż raz. Ile domów otrzymuje co najmniej jeden prezent?
+Na przykład:
+
+^ v dostarcza prezenty do 3 domów, ponieważ Mikołaj jedzie na północ, a następnie Robo-Santa jedzie na południe.
+^> v <teraz dostarcza prezenty do 3 domów, a Mikołaj i Robo-Santa kończą z powrotem tam, gdzie zaczęli.
+^ v ^ v ^ v ^ v ^ v dostarcza obecnie prezenty do 11 domów, z Mikołajem idącym w jednym kierunku i Robo-Mikołajem do drugiego.
+
+
+Zadanie 2.
+
+W następnym roku, aby przyspieszyć proces, Mikołaj tworzy robota wersję siebie, Robo-Santa, aby dostarczyć prezenty z nim.
+
+Święty Mikołaj i Robo-Santa zaczynają w tym samym miejscu (dostarczając dwa prezenty do tego samego domu startowego), a następnie ruszają się na zmianę zgodnie z instrukcjami elfa, który egiptologicznie czyta z tego samego scenariusza, co poprzedni rok.
+
+W tym roku, ile domów otrzymuje co najmniej jeden prezent?
+
+Na przykład:
+
+^ v dostarcza prezenty do 3 domów, ponieważ Mikołaj jedzie na północ, a następnie Robo-Santa jedzie na południe.
+^> v <teraz dostarcza prezenty do 3 domów, a Mikołaj i Robo-Santa kończą z powrotem tam, gdzie zaczęli.
+^ v ^ v ^ v ^ v ^ v dostarcza obecnie prezenty do 11 domów, z Mikołajem idącym w jednym kierunku i Robo-Mikołajem do drugiego.
+
+
+Zadanie 3.
+
+Arkusz kalkulacyjny składa się z rzędów liczb pozornie losowych. Aby upewnić się, że proces odzyskiwania jest prawidłowy, musisz obliczyć sumę kontrolną arkusza kalkulacyjnego. Dla każdego rzędu określ różnicę między największą wartością a najmniejszą wartością; suma kontrolna jest sumą wszystkich tych różnic.
+
+Na przykład, biorąc pod uwagę następujący arkusz kalkulacyjny:
+
+5 1 9 5
+7 5 3
+2 4 6 8
+
+Największe i najmniejsze wartości pierwszego rzędu to 9 i 1, a różnica ich wynosi 8.
+Największe i najmniejsze wartości w drugim rzędzie to 7 i 3, a różnica między nimi to 4.
+Różnica trzeciego rzędu to 6.
+
+W tym przykładzie suma kontrolna arkusza kalkulacyjnego wyniesie 8 + 4 + 6 = 18.
+
+Jaka jest suma kontrolna arkusza kalkulacyjnego w danych wejściowych puzzli?
