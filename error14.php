08d1067ef83800cd2b5e44c75a2ae4018bc68ff6
Michal Obielak <mobielak7@gmial.com>
2018-07-04 16:19:53 +0200

    wpisany warunek do wyświetlenia var_dump

---
+++ error14.php
@@ -0,0 +1,51 @@
+<?php
+error_reporting(E_ALL);
+
+
+class A {
+    private $test = 'test';
+    public function test()
+    {
+        return date('H:m:s');
+    }
+}
+class B extends A {
+
+    public function test()
+    {
+        return date('H:m:s');
+    }
+}
+class E extends A {
+
+    public function test()
+    {
+        return date('H:m:s');
+    }
+}
+class D extends A {
+
+    public function test()
+    {
+        return date('H:m:s');
+    }
+}
+class C extends A {
+
+    public function test()
+    {
+        return date('H:m:s');
+    }
+}
+class G {
+
+    public function test()
+    {
+        return date('H:m:s');
+    }
+}
+$name = $_GET['name'];
+if((($name>='A')&&($name<='E'))||($name=='G')){
+var_Dump((new $name())->test());}else{
+	echo "brak";}
+	
