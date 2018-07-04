5b304409b5570727d35107d3a186cf4fcd16d977
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-04 14:16:37 +0200

    Dodanie parametru do trigger_error w error3.php

--- error/error3.php
+++ error/error3.php
@@ -3,7 +3,7 @@
 error_reporting(E_ALL);
 
 if (!isset($value)) {
-    trigger_error('Zmienna $value nie jest zdefiniowana');
+    trigger_error('Zmienna $value nie jest zdefiniowana', E_USER_WARNING);
 }
 
 var_Dump($value['a']['b']);
