604b7a808a8e2d22bf8539d6e259f3cbd8831c24
Michal Obielak <mobielak7@gmial.com>
2018-07-04 13:35:38 +0200

    zabezpieczenie formularzy

--- tabelka.php
+++ tabelka.php
@@ -2,8 +2,9 @@
 	include("connect.php");
 
 	$i=$_GET['id'];
-	$zap="select * from clients where id=$i";
-	$wynik=mysqli_query($link,$zap);
-	while($w=mysqli_fetch_row($wynik)){
-		echo $w[1]." ".$w[2];}
+	$zap='delete * from clients where id='.$i;
+	echo $zap;
+	
+	mysqli_query($link,$zap);
+	
 ?>
