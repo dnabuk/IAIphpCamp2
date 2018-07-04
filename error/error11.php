cc8d47d7c4da6d4a23caae9dd830d6c0f2eb8ca9
Pawel Lica <pawel.lica@gmail.com>
2018-07-04 15:36:48 +0200

    Poprawiono sprawdzanie warunków

--- error/error11.php
+++ error/error11.php
@@ -1,4 +1,5 @@
 <?php
+//error_reporting(E_ALL); //na czas poprawek
 error_reporting(0);
 
 
@@ -16,9 +17,17 @@ class A {
 
 
 
-if ($_GET['validateName'] = 1) {
+if ((isset($_GET['validateName'])) && ($_GET['validateName'] == 1)) {
     //sprawdzamy czy przeslany jest parametr odpowiadajacy za walidacje nazwy
     $validateName = true;
 }
-$age = (int)$_GET['age'];
-(new A)->printInfo($_GET['name'], $validateName, $age);
+else {
+	echo 'Złe dane, użyj ?age=WIEK&name=IMIĘ&validateName=1';
+	die;
+}
+if (isset($_GET['age'])){
+	$age = (int)$_GET['age'];
+}
+if (isset($_GET['name'])){
+	(new A)->printInfo($_GET['name'], $validateName, $age);
+}
