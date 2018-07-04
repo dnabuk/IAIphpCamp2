a46f51cace43ea69fac4a95e3bebcf6b75e84e02
Marta Jaworska <maartajaworska@gmail.com>
2018-07-04 15:53:02 +0200

    error poprawiony

--- error/error12.php
+++ error/error12.php
@@ -7,8 +7,7 @@ class A {
     public function printInfo($name, $validateName)
     {
         if ($validateName == true) {
-			//strpos wypisuje pozycje na ktorej znajduje sie dany szablon
-            if ( $name === "Mateusz") {
+            if ( strpos($name, 'Mateusz' ) !== false) {
                 //sprawdzamy czy imie mateusz znajduje sie w przekazanej nazwie
                 echo 'Witaj Mateusz!';
             } else {
