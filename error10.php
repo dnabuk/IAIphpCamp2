e5c403f86729e9d47d24043ad7b3c70e1a09b719
Michal Obielak <mobielak7@gmial.com>
2018-07-04 15:21:18 +0200

    dodanie break do case w linii 14 i 16

---
+++ error10.php
@@ -0,0 +1,23 @@
+<?php
+
+
+$color = $_GET['color'];
+
+
+class A {
+
+    public function getSpan($color, $value)
+    {
+        $html = '';
+        switch ($color) {
+            case 'red':
+                $html = '<span style="color:red">' . $value. '</span>';break;
+            case 'blue':
+                $html = '<span style="color:blue">' . $value. '</span>';break;
+        }
+
+        return $html;
+    }
+}
+
+echo (new A)->getSpan($_GET['color'], 'Kolorowy tekst');
