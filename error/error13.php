b5da4609f0c26957c6fae38823d8fd2b6db35b08
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-04 16:06:21 +0200

    Poprawiony błąd w formacie daty

--- error/error13.php
+++ error/error13.php
@@ -6,12 +6,13 @@ class A {
 
     public function getTime($timestamp)
     {
-        return date('H:m:s',$timestamp);
+        return date('H:i:s',$timestamp);
     }
 }
 
 
 echo 'Wynik dla timestamp = ' . 1464847575 . ' czwartek, 2 czerwca 2016 06:06:15';
 var_dump((new A)->getTime(1464847575));
+echo '<br>';
 echo 'Wynik dla timestamp = ' . 1518614715 . ' środa, 14 lutego 2018 13:25:15';
 var_dump((new A)->getTime(1518614715));
