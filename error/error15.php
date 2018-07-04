c6f438649333b87495c614c350d1c905cf5e2757
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-04 16:29:42 +0200

    Error15.php: edycja funkcji i sposobu wywołania

--- error/error15.php
+++ error/error15.php
@@ -3,17 +3,17 @@ error_reporting(E_ALL);
 
 class A {
     private $prefix = 'Pan(i)';
-    public static function printName($name)
-    {
-        $ret = '';
-        if (strlen($name) > 5) {
-            $ret = $this->prefix . ' ' . $name;
-        } else {
-            $ret = 'Siemanko ' . $name;
-        }
-
-        return $ret;
-    }
+	
+	public function printName2($name) 
+	{
+		if (strlen($name) > 5) {
+			$ret = $this->prefix . ' ' . $name;
+		}
+		else {
+			$ret = 'Siemanko ' . $name;
+		}
+		return $ret;
+	}
 }
 
-echo A::printName($_GET['name']);
+echo (new A)->printName2($_GET['name']);
