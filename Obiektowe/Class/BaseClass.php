14a3e55dc7f69b56e802878c3988271f85fcd262
lukasz.krawczyk <honytowk@gmail.com>
2018-07-04 13:00:45 +0200

    robimy Calle, issety Unsety

---
+++ Obiektowe/Class/BaseClass.php
@@ -0,0 +1,21 @@
+<?php
+
+Class ProductBase
+{
+
+    Private $ProductBaseID;
+    Private $ProductBaseName;
+
+
+    function __construct()
+    {
+        $this->ProductBaseID = 1;
+        $this->ProductBaseName = "Cement";
+
+    }
+
+
+
+
+
+}
