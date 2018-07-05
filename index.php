dc937ef5bd829fc84f5957aba7313df7767e5551
Michal Obielak <mobielak7@gmial.com>
2018-07-05 09:53:39 +0200

    Skrypt z ćwiczenia 1 wykonujący operacje na bazie danych

---
+++ index.php
@@ -0,0 +1,38 @@
+﻿<?php 
+include("connect.php");
+if((isset($_GET['id']))
+	&&(isset($_GET['action']))
+	&&(isset($_GET['name']))
+	&&(isset($_GET['price']))){
+		$id=$_GET['id'];
+		$name=$_GET['name'];
+		$price=$_GET['price'];
+		$action=$_GET['action'];
+		switch ($action) {
+			case 'checkProduct':
+				$zap="select * from products where nazwa='$name'";
+				$wynik=mysqli_query($link,$zap);
+				while($k=mysqli_fetch_assoc($wynik)){
+					echo $k['id']." ". $k['nazwa']." ". $k['cena']."<br/>";}
+				break;
+			case 'addProduct':
+				$zap="insert into products(id,nazwa,cena)values('$id','$name','$price');";
+				mysqli_query($link,$zap);
+				$zap="select * from products;";
+				$wynik=mysqli_query($link,$zap);
+				while($k=mysqli_fetch_assoc($wynik)){
+					echo $k['id']." ". $k['nazwa']." ". $k['cena']."<br/>";}
+				break;
+			
+			case 'removeProduct':
+				$zap="delete from products where id='$id';";
+				mysqli_query($link,$zap);
+				$zap="select * from products;";
+				$wynik=mysqli_query($link,$zap);
+				while($k=mysqli_fetch_assoc($wynik)){
+					echo $k['id']." ". $k['nazwa']." ". $k['cena']."<br/>";}
+				break;
+			};
+		}else{
+			echo "błąd";}
+?>
