5d55335666fc7d43dff957c394f4ab3a3a947175
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-05 11:02:24 +0200

    cwiczenie2.php: json_decode, dodanie parametru true

--- dzien04-cz01/cwiczenie2.php
+++ dzien04-cz01/cwiczenie2.php
@@ -2,9 +2,8 @@
 	$ch = curl_init('http://localhost/iai/03/dzien04-cz01/cwiczenie1.php?action=addProduct&name=Lodowka&product=6&price=1999');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$result = curl_exec($ch);
-	//$response = json_decode($result);
-	$response = $result;
+	$response = json_decode($result, true);
 	echo '<pre>';
-	echo($response);
+	print_r($response);
 	echo '</pre>';
 ?>
