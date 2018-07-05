502dbb092bc9c28f83ea4e7cdeef5b351f338ace
Krzysztof Margański <bystek@o2.pl>
2018-07-05 14:56:27 +0100

    zadanie 2 curl json wyświetlanie i usuwanie działa

--- 4_day/task2.php
+++ 4_day/task2.php
@@ -3,7 +3,7 @@
 	$curl=curl_init('http://localhost/phpcamp2/phpcamp/1_day3/4_day/task3.php/prodactlist');
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'GET');
+	curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'PUT');
 	curl_setopt($curl, CURLOPT_POSTFIELDS, $item);
 	$result=curl_exec($curl);
 	var_dump(json_decode($result,true));
