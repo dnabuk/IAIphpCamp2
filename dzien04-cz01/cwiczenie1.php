788549a8666978013c9f90eced3b6a3d194bbf9f
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-05 09:16:53 +0200

    Utworzona tabela products

--- dzien04-cz01/cwiczenie1.php
+++ dzien04-cz01/cwiczenie1.php
@@ -1,3 +1,37 @@
 <?php
+	$products = array();
 	
+	$products[] = array(
+		'id' => '1',
+		'name' => 'Pluszowy dzięcioł',
+		'price' => 29
+	);
+	
+	$products[] = array(
+		'id' => '2',
+		'name' => 'Pluszowa wiewiórka',
+		'price' => 35
+	);
+	
+	$products[] = array(
+		'id' => '3',
+		'name' => 'Pluszowy kot',
+		'price' => 400
+	);
+	
+	$products[] = array(
+		'id' => '4',
+		'name' => 'Pluszowy ziemniak',
+		'price' => 666
+	);
+	
+	$products[] = array(
+		'id' => '5',
+		'name' => 'Pluszowy burak',
+		'price' => 33
+	);
+	
+	echo '<pre>';
+	print_r($products);
+	echo '</pre>';
 ?>
