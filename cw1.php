4f23b2cd75e7bf83852467e41225e7a55b7597a3
Michal Obielak <mobielak7@gmial.com>
2018-07-05 15:21:46 +0200

    tabela na podstawie bd

---
+++ cw1.php
@@ -0,0 +1,16 @@
+﻿<!doctype html>
+<html>
+	<head>
+	<style>
+	td{
+		border:1px #000 solid;}
+	</style>
+   	</head>
+    <body>
+<?php 
+include_once("connect.php");
+include_once("functions.php");
+wysw();
+?>
+	</body>
+    </html>
---
+++ functions.php
@@ -0,0 +1,22 @@
+﻿<?php
+
+function wysw(){
+	global $link;
+	
+	?>
+	<table>
+    <tr><th>id</th><th>nazwa</th><th>cena</th><th>operacja</th></tr>
+	<?php
+	
+				$zap="select * from products;";
+				$wynik=mysqli_query($link,$zap);
+				while($k=mysqli_fetch_assoc($wynik)){
+					echo "<tr><td>".$k['id']."</td><td>". $k['nazwa']."</td><td>". $k['cena']."</td><td><button>Usuń</button></td></tr>";}
+					
+						?>
+						</table>
+                        <?php
+						}
+						
+						
+?>						
