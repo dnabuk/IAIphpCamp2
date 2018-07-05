8f3f5dd7bcb21bc3186a6d7158842bc30a4de3cc
Krzysztof Margański <bystek@o2.pl>
2018-07-05 09:33:48 +0100

    zadanie 1

---
+++ 4_day/task1.php
@@ -0,0 +1,31 @@
+<?php
+
+$tablica1= array('id' => 1, 'nazwa' => 'Kubek', 'cena' => 200);
+$tablica2= array('id' => 2, 'nazwa' => 'Lampa', 'cena' => 250);
+$tablica3= array('id' => 3, 'nazwa' => 'Rower', 'cena' => 2500);
+$tablica4= array('id' => 4, 'nazwa' => 'Młotek', 'cena' => 25);
+$tablica5= array('id' => 5, 'nazwa' => 'Taca', 'cena' => 2);
+$produkty=[$tablica1,$tablica2,$tablica3,$tablica4,$tablica5];
+if (isset($_GET['action'])) {
+	if ($_GET['action'] == 'checkProduct') {
+		var_dump($produkty);
+	}
+	if ($_GET['action'] == 'addProduct'){
+		$tablica= array('id' => $_GET['id'], 'nazwa' => $_GET['name'], 'cena' => $_GET['price']);
+		$produkty[]=$tablica;
+		var_dump($produkty);
+
+	}
+	if ($_GET['action'] == 'removeProduct') {
+		foreach ($produkty as $key => $value) {
+			if ($value['id'] == $_GET['id']) {
+				unset($produkty[$key]);
+				var_dump($produkty);
+			}
+		}
+	}
+
+
+
+	
+}
