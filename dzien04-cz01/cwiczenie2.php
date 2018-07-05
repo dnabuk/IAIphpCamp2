08bdb94a6e0fbbb71285c304717a27288e4da1ac
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-05 10:50:22 +0200

    json_decode w cwiczenie2.php

--- dzien04-cz01/cwiczenie2.php
+++ dzien04-cz01/cwiczenie2.php
@@ -2,5 +2,7 @@
 	$ch = curl_init('http://localhost/iai/03/dzien04-cz01/cwiczenie1.php?action=addProduct&name=Lodowka&product=6&price=1999');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$result = curl_exec($ch);
-	print_r($result);
+	echo '<pre>';
+	print_r(json_decode($result));
+	echo '</pre>';
 ?>
