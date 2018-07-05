b8afca0d2fd4bf7339887143c29269e6827d85bd
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-05 11:00:02 +0200

    Wyświetlenie w cwiczenie2 danych w formacie json

--- dzien04-cz01/cwiczenie2.php
+++ dzien04-cz01/cwiczenie2.php
@@ -2,8 +2,9 @@
 	$ch = curl_init('http://localhost/iai/03/dzien04-cz01/cwiczenie1.php?action=addProduct&name=Lodowka&product=6&price=1999');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$result = curl_exec($ch);
-	$response = json_decode($result);
+	//$response = json_decode($result);
+	$response = $result;
 	echo '<pre>';
-	print_r($response);
+	echo($response);
 	echo '</pre>';
 ?>
