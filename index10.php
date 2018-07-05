9375c3f37784702da82cddfbeb4ed616cc0f27f4
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-05 12:01:47 +0200

    Pobieranie danych z API przy pomocy curl

--- index10.php
+++ index10.php
@@ -6,15 +6,20 @@ if (isset($_GET['action'])) {
     $action = $_GET['action'];
 }
 
-$ch = curl_init('http://localhost/phppc/index9.php?action='.$action.'&product='.$product.'');
+//$ch = curl_init('http://localhost/phppc/index9.php?action='.$action.'&product='.$product.'');
+$ch = curl_init('http://localhost/phppc/index11.php')
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
 //echo $result; // wynik w formacie JSON
 //echo '<br>';
 //var_dump(json_decode($result,true)); //wynik przekonwertowany z JSONa na PHP array
 $dd =json_decode($result,true);
-foreach ($dd as $d){
-    echo 'User id: '.$d['id'].'<br>';
-    echo 'Name: '.$d['name'].'<br>';
-    echo 'Price: '.$d['price'].'<br>';
+//echo $dd;
+if(isset($dd)){
+    foreach ($dd as $d){
+        echo 'User id: '.$d['id'].'<br>';
+        echo 'Name: '.$d['name'].'<br>';
+        echo 'Price: '.$d['price'].'<br>';
+    }
 }
+
---
+++ index11.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once('config.php');
+$id = 1;
+
+//echo json_encode($result);
+//$products = $result;
+
+$path = $_SERVER['PATH_INFO'];
+$pathArray = explode('/', $path);
+
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ($method) {
+    case 'GET':
+        //echo json_encode($products);
+        //var_dump($pathArray);
+        if(isset($pathArray)){
+
+            $sql = 'SELECT * FROM `products` WHERE id = ?';
+            $c = GetPDO()->prepare($sql);
+            $c->bindParam(1, $pathArray[2]);
+            $c->execute();
+            $result = $c->fetchAll(PDO::FETCH_ASSOC); //poprawka do poprawnego pobierania danych
+            foreach ($result as $row) {
+                echo 'Product id: '.$row['id'].'<br>';
+                echo 'Name: '.$row['name'].'<br>';
+                echo 'Price: '.$row['price'].'<br>';
+            }
+            //var_dump($result);
+        }
+        break;
+    case 'PUT':
+        $params = json_decode(file_get_contents('php://input'),1);
+        break;
+    case 'POST':
+        $params = json_decode(file_get_contents('php://input'),1);
+        break;
+    case 'DELETE':
+        break;
+}
+//print_r($pathArray);
+//echo json_encode($products);
