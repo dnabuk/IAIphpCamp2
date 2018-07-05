02a48a81a1f58c0d2e7b9dc71a2187b0117465a8
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-05 09:36:48 +0200

    Dodanie metod dodawania i usuwania rekordów w bazie

--- index9.php
+++ index9.php
@@ -30,7 +30,18 @@ if(isset($_GET['action'])){
         echo 'Wyświetlanie produktu o id: ';
         checkProduct();
     }else if ($_GET['action'] == 'addProduct'){
+        $sql = 'INSERT INTO `products`(`name`, `price`) VALUES (?,?)';
+        $c = GetPDO()->prepare($sql);
+        $c->bindParam(1,$_GET['name']);
+        $c->bindParam(2,$_GET['price']);
+        $c->execute();
+    } else if ($_GET['action'] == 'deleteProduct'){
 
+            $sql = 'DELETE FROM `products` WHERE `products`.`id` = ?';
+        $c = GetPDO()->prepare($sql);
+        $c->bindParam(1,$_GET['product']);
+        $c->execute();
+        
     }
 
 }
