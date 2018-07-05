6239109371c68dfbd54b20c624c24f75928890b4
Michal Obielak <mobielak7@gmial.com>
2018-07-05 13:32:03 +0200

    rest get vol.2

--- api.php
+++ api.php
@@ -1,16 +1,24 @@
 ﻿<?php
-$product=array('id1'=>array('1','jajka','4'),
-				'id2'=>array('2','mleko','2'));
 
-$path=$_SERVER['PATH_INFO'];
+$products=array(array('id'=>'1','nazwa'=>'jajka','cena'=>'4'),
+				array('id'=>'2','nazwa'=>'mleko','cena'=>'2'));
+
+$path=$_SERVER['REQUEST_URI'];
+//var_dump($_SERVER);die;
 $pathArray=explode('/',$path);
-print_r($pathArray);
-echo "<br/>";
+$kolekcja=$pathArray[3];
+$nazwa=$pathArray[4];
+
 $method=$_SERVER['REQUEST_METHOD'];
 
 switch($method){
 	case 'GET':
-		//
+		if($kolekcja=='productsList'){
+			foreach ($products as $produkt){
+				if($produkt['nazwa']==$nazwa){
+					var_dump($produkt);}
+				}
+			}
 		break;
 	case 'PUT':
 		$params=json_decode(file_get_contents('php://input'),1);
@@ -23,5 +31,5 @@ switch($method){
 		break;
 	default: break;
 	}
-echo json_encode($product);
+echo json_encode($products);
 ?>
--- get.php
+++ get.php
@@ -1,11 +1,8 @@
 ﻿<?php 
-$ch=curl_init('http://localhost/phpcamp%205.07/api.php/productlist/id2');
-curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-$result =curl_exec($ch);
+$ch=curl_init('http://localhost/phpcamp%205.07/api.php/productList/mleko');
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+$response =curl_exec($ch);
+$productsList=json_decode($response,1);
 
-print_r($result);
-echo "<br/>";
-$z=json_decode($result,true);
-
-print_r($z);
+print_r($productsList);die;
 ?>
