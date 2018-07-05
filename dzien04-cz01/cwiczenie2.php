c97e948ee78943cf37ce1432c9fbc5eaa0a43f97
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-05 10:38:10 +0200

    cURL - przykład: cwiczenie2.php

---
+++ dzien04-cz01/cwiczenie2.php
@@ -0,0 +1,6 @@
+<?php
+	$ch = curl_init('http://localhost/iai/03/dzien04-cz01/cwiczenie1.php?action=addProduct&name=Lodowka&product=6&price=1999');
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($ch);
+	print_r($result);
+?>
