d8f236311d2de93199a3515b7e7338ad900506fd
Michał Zakrzewski <michalzakrzewski@users.noreply.github.com>
2018-07-05 10:45:40 +0200

    only rename

--- pl.phpcamp/DzienCzwart_ApiRestAJAX/Cwiczenie2.php
+++ pl.phpcamp/DzienCzwart_ApiRestAJAX/Cwiczenie2.php
@@ -1,5 +1,8 @@
 <?php
-$url = curl_init('http://localhost/Camp2/pl.phpcamp/DzienCzwart_ApiRestAJAX/Cwiczenie1.php?action=checkProduct&id=1&nazwa=Kubek&cena=30');
-curl_setopt($url, CURLOPT_RETURNTRANSFER,true); // nie ma byc automatycznie tylko do zmiennej
-$result = curl_exec($url);
-print_r($result);
+
+function onClick(){
+    $url = curl_init('http://localhost/Camp2/pl.phpcamp/DzienCzwart_ApiRestAJAX/Cwiczenie1.php?action=checkProduct&id=1&nazwa=Kubek&cena=30');
+    curl_setopt($url, CURLOPT_RETURNTRANSFER,true); // nie ma byc automatycznie tylko do zmiennej
+    $result = curl_exec($url);
+    print_r($result);
+}
