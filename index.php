d140e3e6d3d2760b5a096b5951f514cd2ad7c550
Michal Obielak <mobielak7@gmial.com>
2018-07-05 10:06:06 +0200

    dodanie funkcji do wyświetlania

--- index.php
+++ index.php
@@ -1,5 +1,10 @@
 ﻿<?php 
 include("connect.php");
+function wysw(){
+	$zap="select * from products;";
+				$wynik=mysqli_query($link,$zap);
+				while($k=mysqli_fetch_assoc($wynik)){
+					echo $k['id']." ". $k['nazwa']." ". $k['cena']."<br/>";}}
 if((isset($_GET['id']))
 	&&(isset($_GET['action']))
 	&&(isset($_GET['name']))
@@ -18,19 +23,13 @@ if((isset($_GET['id']))
 			case 'addProduct':
 				$zap="insert into products(id,nazwa,cena)values('$id','$name','$price');";
 				mysqli_query($link,$zap);
-				$zap="select * from products;";
-				$wynik=mysqli_query($link,$zap);
-				while($k=mysqli_fetch_assoc($wynik)){
-					echo $k['id']." ". $k['nazwa']." ". $k['cena']."<br/>";}
+				wysw();
 				break;
 			
 			case 'removeProduct':
 				$zap="delete from products where id='$id';";
 				mysqli_query($link,$zap);
-				$zap="select * from products;";
-				$wynik=mysqli_query($link,$zap);
-				while($k=mysqli_fetch_assoc($wynik)){
-					echo $k['id']." ". $k['nazwa']." ". $k['cena']."<br/>";}
+				wysw();
 				break;
 			default:echo"zła akcja<br/>";break;
 			};
