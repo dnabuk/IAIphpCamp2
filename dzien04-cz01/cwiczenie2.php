0c8ac81a83259d9e901b86b2ec65606c3d542db4
Marlena Adamczak <marlena.adamczak@gmail.com>
2018-07-05 10:54:41 +0200

    json_decode w cwiczenie2.php - przypisanie do zmiennej

--- dzien04-cz01/cwiczenie2.php
+++ dzien04-cz01/cwiczenie2.php
@@ -2,7 +2,8 @@
 	$ch = curl_init('http://localhost/iai/03/dzien04-cz01/cwiczenie1.php?action=addProduct&name=Lodowka&product=6&price=1999');
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	$result = curl_exec($ch);
+	$response = json_decode($result);
 	echo '<pre>';
-	print_r(json_decode($result));
+	print_r($response);
 	echo '</pre>';
 ?>
