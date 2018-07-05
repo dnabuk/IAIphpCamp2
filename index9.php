c055de5a65b9fd910e1f6d7d732f8fe6f010e72a
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-05 09:51:57 +0200

    Posprzątanie kodu; dodanie rzutowanie typów

--- index9.php
+++ index9.php
@@ -1,53 +1,48 @@
 <?php
 /*
  *
+ * Zadanie - napisanie skryptu który pobiera, tworzy i usuwa dane z tablicy/bazy
  *
- *
- *
+ * Dzień 4
  */
 
 require_once('config.php');
 
-$product = $_GET['product'];
+$product = intval($_GET['product']);
 $action = $_GET['action'];
 $name = $_GET['name'];
-$price = $_GET['price'];
+$price = (float)$_GET['price'];
 
-function checkProduct(){
+function checkProduct()
+{
     $sql = 'SELECT * FROM `products` WHERE id = ?';
     $c = GetPDO()->prepare($sql);
-    $c->bindParam(1,$_GET['product']);
+    $c->bindParam(1, $_GET['product']);
     $c->execute();
     $result = $c->fetchAll();
-    foreach($result as $row) {
-        echo $row['id'].'<br>';
-        echo $row['name'].'<br>';
-        echo $row['price'].'<br>';
+    foreach ($result as $row) {
+        echo $row['id'] . '<br>';
+        echo $row['name'] . '<br>';
+        echo $row['price'] . '<br>';
     }
 }
-if(isset($_GET['action'])){
-    if($_GET['action'] == 'checkProduct'){
+
+if (isset($_GET['action'])) {
+    if ($_GET['action'] == 'checkproduct') {
         echo 'Wyświetlanie produktu o id: ';
         checkProduct();
-    }else if ($_GET['action'] == 'addProduct'){
+    } else if ($_GET['action'] == 'addProduct') {
         $sql = 'INSERT INTO `products`(`name`, `price`) VALUES (?,?)';
         $c = GetPDO()->prepare($sql);
-        $c->bindParam(1,$_GET['name']);
-        $c->bindParam(2,$_GET['price']);
+        $c->bindParam(1, $_GET['name']);
+        $c->bindParam(2, $_GET['price']);
         $c->execute();
-    } else if ($_GET['action'] == 'deleteProduct'){
-
-            $sql = 'DELETE FROM `products` WHERE `products`.`id` = ?';
+    } else if ($_GET['action'] == 'deleteProduct') {
+        $sql = 'DELETE FROM `products` WHERE `products`.`id` = ?';
         $c = GetPDO()->prepare($sql);
-        $c->bindParam(1,$_GET['product']);
+        $c->bindParam(1, $_GET['product']);
         $c->execute();
-        
+
     }
 
 }
-
-
-
-//function api('product',$_GET['action'],'name','price'){
-
-//}
