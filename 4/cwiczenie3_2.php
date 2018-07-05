13c79337b1a78f7c76f36dd202aeeab0e9bda97c
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2018-07-05 16:49:55 +0200

    delete unwanted code

--- 4/cwiczenie3_2.php
+++ 4/cwiczenie3_2.php
@@ -3,7 +3,6 @@ $ch = curl_init('http://localhost/PHPCamp/4/cwiczenie3_1.php');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result=curl_exec($ch);
 //print_r($result);
-//$response=json_decode($response,true);
 
-echo $result;
-//var_dump(json_decode($result,true));
+//echo $result;
+var_dump(json_decode($result,true));
