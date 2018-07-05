f76b790c4f659966d0d019816afe9ab246e00fc7
Michal Obielak <mobielak7@gmial.com>
2018-07-05 12:11:44 +0200

    rest get

---
+++ api.php
@@ -0,0 +1,27 @@
+﻿<?php
+$product=array('id1'=>array('1','jajka','4'),
+				'id2'=>array('2','mleko','2'));
+
+$path=$_SERVER['PATH_INFO'];
+$pathArray=explode('/',$path);
+print_r($pathArray);
+echo "<br/>";
+$method=$_SERVER['REQUEST_METHOD'];
+
+switch($method){
+	case 'GET':
+		//
+		break;
+	case 'PUT':
+		$params=json_decode(file_get_contents('php://input'),1);
+		break;
+	case 'POST':
+		$params=json_decode(file_get_contents('php://input'),1);
+		break;
+	case 'DELETE':
+		//
+		break;
+	default: break;
+	}
+echo json_encode($product);
+?>
---
+++ get.php
@@ -0,0 +1,11 @@
+﻿<?php 
+$ch=curl_init('http://localhost/phpcamp%205.07/api.php/productlist/id2');
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$result =curl_exec($ch);
+
+print_r($result);
+echo "<br/>";
+$z=json_decode($result,true);
+
+print_r($z);
+?>
