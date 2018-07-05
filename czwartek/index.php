8de48002a51d5c167ce9a5fff5f8afce8ccb64e5
Pawel Lica <pawel.lica@gmail.com>
2018-07-05 11:00:55 +0200

    Zmiana wyświetlania odebranych danych

--- czwartek/index.php
+++ czwartek/index.php
@@ -4,8 +4,12 @@ $id=2;
 $ch = curl_init('http://localhost/czwartek/api.php?action='.$action.'&product='.$id);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
-print_r($result);
+print_r('JSON: '.$result);
+
+echo '<br>Array: ';
+
+$tab = json_decode($result);
+print_r($tab);
 
 echo '<br>';
-
-print_r(json_decode($result));
+foreach ($tab as $val) echo $val.' ';
