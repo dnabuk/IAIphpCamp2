fc7382cb08301300413451cd63b6158da3d171f0
Pawel Lica <pawel.lica@gmail.com>
2018-07-05 10:48:25 +0200

    Proste api

--- czwartek/index.php
+++ czwartek/index.php
@@ -1,53 +1,7 @@
 <?php
-
-if (isset($_GET['action'])){
-	$action = $_GET['action'];
-	if (isset($_GET['name'])){
-		$name = $_GET['name'];
-	} else {
-		$name='';
-	}
-	if (isset($_GET['price'])){
-		$price = $_GET['price'];
-	} else {
-		$price=0;
-	}
-	if (isset($_GET['product'])){
-		$id=$_GET['product'];
-	}
-} else {
-	echo 'Błąd';
-	die;
-}
-
-
-$link = mysqli_connect("localhost", "php2", "jcSxS71poOmxo4pu", "Czwartek");
-if ($link->connect_errno){
-	echo 'Błąd połączenia z bazą';
-	die;
-}
-
-$res='';
-switch ($action){
-	case 'checkProduct':	$stmt = $link->prepare("SELECT * FROM `products` WHERE `id` = ?");
-							$stmt->bind_param('i', $id);
-							$stmt->execute();
-
-							$result = $stmt->get_result();
-							
-							while ($row = $result->fetch_assoc()) {
-								$res = json_encode($row);
-							}
-							break;
-	
-	case 'addProduct':		$stmt = $link->prepare("INSERT INTO `products` (`id`, `nazwa`, `cena`) VALUES (?, '?', '?')");
-							$stmt->bind_param('isi', $id, $name, $price);
-							$stmt->execute();
-	
-	case 'removeProduct':	$stmt = $link->prepare("DELETE FROM `products` WHERE `products`.`id` = ?");
-							$stmt->bind_param('i', $id);
-							$stmt->execute();
-							break;
-	default:				echo 'błędne dane';
-}
-echo $res;
+$action='checkProduct';
+$id=2;
+$ch = curl_init('http://localhost/czwartek/api.php?action='.$action.'&product='.$id);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$result = curl_exec($ch);
+print_r($result);
