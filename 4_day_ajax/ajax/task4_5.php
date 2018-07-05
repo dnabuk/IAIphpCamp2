2864dcc7b236bbf2a5a29a2ff86f1ae73be9b9db
Krzysztof Margański <bystek@o2.pl>
2018-07-05 15:30:36 +0100

    ajax tabela usuń

--- 4_day_ajax/ajax/task4_5.php
+++ 4_day_ajax/ajax/task4_5.php
@@ -1,9 +1,9 @@
 <?php
 require_once 'connect.php';
 
-if (isset($_GET['action'])) {
-	if ($_GET['action'] == 'checkProduct') {
-		if (!isset($_GET['name'])) {
+if (isset($_POST['action'])) {
+	if ($_POST['action'] == 'checkProduct') {
+		if (!isset($_POST['name'])) {
 			echo json_encode("Podaj nazwę produktu");
 			die();
 		}
@@ -20,13 +20,13 @@ if (isset($_GET['action'])) {
 			}
 		}
 	}
-	if ($_GET['action'] == 'addProduct'){
-		if (!isset($_GET['name'])) {
-			echo json_encode("Podaj nazwę produktu");
+	if ($_POST['action'] == 'addProduct'){
+		if (!isset($_POST['name'])) {
+			echo("Podaj nazwę produktu");
 			die();
 		}
-		if (!isset($_GET['price'])) {
-			echo json_encode("Podaj nazwę produktu");
+		if (!isset($_POST['price'])) {
+			echo("Podaj nazwę produktu");
 			die();
 		}
 		$name = mysqli_real_escape_string($link,htmlentities($_GET['name']));
@@ -35,28 +35,22 @@ if (isset($_GET['action'])) {
 		if(!$result= mysqli_query($link,$query_ins)){
 			echo (mysqli_error($link));
 		}else{
-			echo json_encode('wysłano');
+			//echo json_encode('wysłano');
 		}
-		$query='SELECT * from `product`';
-		if($result= mysqli_query($link,$query)){
-			while ($data= mysqli_fetch_assoc($result)) {
-				echo json_encode($data);
-				//var_dump($data);
-			}
-		}
+		
 	}
 
 	
-	if ($_GET['action'] == 'removeProduct') {
-		if (!isset($_GET['id']) && !empty($_GET['id'])) {
+	if ($_POST['action'] == 'removeProduct') {
+		if (!isset($_POST['id']) && !empty($_GET['id'])) {
 			echo "Podaj id";
 			die();
 		}
-		$id=(int)$_GET['id'];
+		$id=(int)$_POST['id'];
 		$query="SELECT * from `product` WHERE id='$id'";
 		if($result= mysqli_query($link,$query)){
 			if (mysqli_num_rows($result)==0) {
-				echo json_encode('Brak porduktu o podanym id');
+				echo('Brak porduktu o podanym id');
 				die();
 			}
 
@@ -66,16 +60,23 @@ if (isset($_GET['action'])) {
 		if(!$result= mysqli_query($link,$query_ins)){
 			echo (mysqli_error($link));
 		}else{
-			echo json_encode('wysłano');
+			echo('wysłano');
 		}
-		$query='SELECT * from `product`';
-		if($result= mysqli_query($link,$query)){
-			while ($data= mysqli_fetch_assoc($result)) {
-				echo json_encode($data);
-				//var_dump($data);
-			}
-		}
+		
 	}
+	$query='SELECT * from `product`';
+		if($result= mysqli_query($link,$query)){
+			echo "<table border=1>";
+	echo "<tr><td>id</td><td>name</td><td>price</td><td>atcion</td></tr>";
+
+			while ($data= mysqli_fetch_assoc($result)) {
+				//echo json_encode($data);
+				//var_dump($data);
+				echo '<tr><td>'.$data['id'].'</td><td>'.$data['nazwa'].'</td><td>'.$data['cena'].'</td><td><button type="button" onclick="del('.$data['id'].')">usuń</button></td></tr>';			
+			}
+			echo "</table>";
+		}
+	
 }else{
 	$query='SELECT * from `product`';
 		if($result= mysqli_query($link,$query)){
@@ -85,7 +86,7 @@ if (isset($_GET['action'])) {
 			while ($data= mysqli_fetch_assoc($result)) {
 				//echo json_encode($data);
 				//var_dump($data);
-				echo '<tr><td>'.$data['id'].'</td><td>'.$data['nazwa'].'</td><td>'.$data['cena'].'</td><td></td></tr>';			
+				echo '<tr><td>'.$data['id'].'</td><td>'.$data['nazwa'].'</td><td>'.$data['cena'].'</td><td><button type="button" onclick="del('.$data['id'].')">usuń</button></td></tr>';			
 			}
 			echo "</table>";
 		}
