4f994e86012d65ef668cac9652dccc978dd71172
Piotr <pittero1@wp.pl>
2018-07-05 09:46:39 +0200

    dzien 4 - api i ajax v2

--- dzień 4/index1.php
+++ dzień 4/index1.php
@@ -1,3 +1,6 @@
+<a href="./index1.php?product=1&action=checkProduct">Pokaz produkt o ID 1</a><br/>
+<a href="./index1.php?action=addProduct&name=piwo&price=13.00">Dodaj produkt</a><br/>
+<a href="./index1.php?product=1&action=removeProduct">Usun produkt o ID</a><br/>
 <?php
 $servername = 'localhost';
 $username = 'root';
@@ -10,31 +13,54 @@ if ($conn->connect_error) {
 } 
 
 
-$product = $_GET['product'];
-$action = $_GET['action'];
-$name = $_GET['name'];
-$price = $_GET['price'];
+$product = isset($_GET['product']) ? $_GET['product'] : null;
+$action = isset($_GET['action']) ? $_GET['action'] : null;
+$name = isset($_GET['name']) ? $_GET['name'] : null;
+$price = isset($_GET['price']) ? $_GET['price'] : null;
 
-if(isset($product) && isset($action) && isset($name) && isset($price))
+
+if(isset($product) && ($action == 'checkProduct'))
 {
-		if($action == 'checkProduct')
+	$sql = "SELECT * FROM product WHERE id = '$product' LIMIT 1";
+	$result = $conn->query($sql);
+	$wynik = $result->fetch_assoc();
+	print_r($wynik);
+	
+}
+else if(($action == 'addProduct') && isset($name) && isset($price))
+{
+	$sql = "INSERT INTO `product` (`id`, `name`, `price`)
+	VALUES (NULL, '$name', '$price');";
+	$result = $conn->query($sql);
+	$id = $conn->insert_id;
+	
+	$sql = "SELECT * FROM product WHERE id = '$id' LIMIT 1";
+	$result = $conn->query($sql);
+	$wynik = $result->fetch_assoc();
+	print_r($wynik);	
+}
+else if($action == 'removeProduct')
+{
+	$sql = "SELECT *, COUNT(id) AS ilosc FROM product WHERE id = '$product' LIMIT 1";
+	$result = $conn->query($sql);
+	$wynik = $result->fetch_assoc();
+	if($wynik['ilosc'] === '1')
+	{
+		$sql = "DELETE FROM product WHERE id = '$product' LIMIT 1";
+		if ($conn->query($sql) === TRUE)
 		{
-			$sql = "SELECT * FROM product WHERE product_id = '$product'";
-			$wynik = $result->fetch_assoc();
+			echo 'popranie usunięto rekord o id: '.$product;
 			print_r($wynik);
-			
 		}
-		else if($action == 'addProduct')
-		{
-			
-		}
-		else if($action == 'removeProduct')
-		{
-			
-		}
-
 		else
 		{
-			
+			echo 'blad usuwania';
 		}
+	}
+
+	
+}
+else
+{
+	echo 'Niepoprawna akcja';
 }
