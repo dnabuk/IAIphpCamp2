3c61e3894a3741d8fe82fd952d25e58c17d152f6
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2018-07-05 10:44:28 +0200

    API Curl working

--- 4/cwiczenie2.php
+++ 4/cwiczenie2.php
@@ -1,5 +1,5 @@
 <?php
-$ch = curl_init('http://localhost/PHPCamp/4/cwiczenie1.php?action=chheckProduct');
+$ch = curl_init('http://localhost/PHPCamp/4/cwiczenie1.php?action=checkProduct&name=komputer');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result=curl_exec($ch);
 print_r($result);
