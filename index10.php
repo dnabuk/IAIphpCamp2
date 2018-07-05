8e61875e59c2157df449860cdab77f9f1d71471a
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-05 11:07:01 +0200

    Zamiana danych z JSON na tablice PHP

--- index10.php
+++ index10.php
@@ -3,4 +3,11 @@
 $ch = curl_init('http://localhost/phppc/index9.php?action=show&product=5');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 $result = curl_exec($ch);
-echo $result;
+//echo $result; // wynik w formacie JSON
+//echo '<br>';
+//var_dump(json_decode($result,true)); //wynik przekonwertowany z JSONa na PHP array
+//$dd =json_decode($result,true);
+echo '<br>';
+foreach ($dd as $d){
+    echo $d['id'];
+}
