2ca1977725be7f5f5c40cc45cf1b01f7b2b2bd55
Michal Obielak <mobielak7@gmial.com>
2018-07-05 10:39:52 +0200

    ćwiczenie 2- curl

---
+++ curl.php
@@ -0,0 +1,6 @@
+﻿<?php 
+$ch=curl_init('http://localhost/phpcamp%205.07/index.php?id=1&action=checkProduct&name=kolejny_rower&price=2000');
+curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+$result =curl_exec($ch);
+print_r($result);
+?>
