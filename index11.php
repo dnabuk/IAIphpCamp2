d82b9988d5522ff8231b65c0ad6eedd691b3b5ba
Hubert Rzadzinski <h.rzadzinski@gmail.com>
2018-07-05 12:06:16 +0200

    Ustawienie warunku wywołania API

--- index11.php
+++ index11.php
@@ -16,18 +16,20 @@ switch ($method) {
         //echo json_encode($products);
         //var_dump($pathArray);
         if(isset($pathArray)){
-
-            $sql = 'SELECT * FROM `products` WHERE id = ?';
-            $c = GetPDO()->prepare($sql);
-            $c->bindParam(1, $pathArray[2]);
-            $c->execute();
-            $result = $c->fetchAll(PDO::FETCH_ASSOC); //poprawka do poprawnego pobierania danych
-            foreach ($result as $row) {
-                echo 'Product id: '.$row['id'].'<br>';
-                echo 'Name: '.$row['name'].'<br>';
-                echo 'Price: '.$row['price'].'<br>';
+            if($pathArray[1] == 'products'){
+                $sql = 'SELECT * FROM `products` WHERE id = ?';
+                $c = GetPDO()->prepare($sql);
+                $c->bindParam(1, $pathArray[2]);
+                $c->execute();
+                $result = $c->fetchAll(PDO::FETCH_ASSOC); //poprawka do poprawnego pobierania danych
+                foreach ($result as $row) {
+                    echo 'Product id: '.$row['id'].'<br>';
+                    echo 'Name: '.$row['name'].'<br>';
+                    echo 'Price: '.$row['price'].'<br>';
+                }
+                //var_dump($result);
             }
-            //var_dump($result);
+
         }
         break;
     case 'PUT':
